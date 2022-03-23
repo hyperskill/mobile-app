9d85fd7a3838fbc2fb12d9fdbeae9425a79c1d29
Daniil Lyubaev <lyubaevda@gmail.com>
2022-03-23 21:13:31 +0300

    Add Google Sign In with ActivityResultContracts

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/auth/view/ui/fragment/AuthSocialFragment.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/auth/view/ui/fragment/AuthSocialFragment.kt
@@ -1,25 +1,22 @@
 package org.hyperskill.app.android.auth.view.ui.fragment
 
-import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.ProgressBar
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.Scopes
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.Scope
-import com.google.android.gms.tasks.Task
-import com.google.android.material.snackbar.Snackbar
 import org.hyperskill.app.android.BuildConfig
 import org.hyperskill.app.android.R
 import org.hyperskill.app.android.databinding.FragmentAuthSocialBinding
 import org.hyperskill.app.auth.presentation.AuthFeature
 import ru.nobird.android.view.base.ui.delegate.ViewStateDelegate
 import ru.nobird.app.presentation.redux.container.ReduxView
-import by.kirich1409.viewbindingdelegate.viewBinding
 
 class AuthSocialFragment
     : Fragment(R.layout.fragment_auth_social),
@@ -29,49 +26,38 @@ class AuthSocialFragment
     private val viewBinding by viewBinding(FragmentAuthSocialBinding::bind)
     private lateinit var viewStateDelegate: ViewStateDelegate<AuthFeature.State>
 
+    private val signInWithGoogleCallback = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {res ->
+        val task = GoogleSignIn.getSignedInAccountFromIntent(res.data)
+        try {
+            val account = task.getResult(ApiException::class.java)
+            val authCode = account.serverAuthCode
+            Log.d("GOOGLE_AUTH", authCode)
+        } catch (e: ApiException) {}
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewStateDelegate = ViewStateDelegate()
-        viewStateDelegate.addState<AuthFeature.State.Idle>()
-        viewStateDelegate.addState<AuthFeature.State.Loading>(viewBinding.signInSocialProgressIndicator)
 
         viewBinding.signInSocialLoginButton.setOnClickListener {
             viewStateDelegate.switchState(AuthFeature.State.Loading)
-            signInWithGoogle(requireActivity())
+            signInWithGoogle()
         }
     }
 
     override fun onAction(action: AuthFeature.Action.ViewAction) {}
 
-    override fun render(state: AuthFeature.State) {
-        Snackbar.make(requireView(), "Login error", Snackbar.LENGTH_LONG)
-            .setAction("Retry") {
-            }
-            .show()
-        if (state is AuthFeature.State.NetworkError) {
-            // TODO: add text and action depending on error
-            Snackbar.make(requireView(), "Login error", Snackbar.LENGTH_LONG)
-                .setAction("Retry") {
-                }
-                .show()
-        }
-    }
+    override fun render(state: AuthFeature.State) {}
 
-    private fun signInWithGoogle(activity: Activity) {
+    private fun signInWithGoogle() {
         val serverClientId = BuildConfig.SERVER_CLIENT_ID
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestScopes(Scope(Scopes.EMAIL), Scope(Scopes.PROFILE))
             .requestServerAuthCode(serverClientId)
             .build()
-        try {
-            val mGoogleSignInClient = GoogleSignIn.getClient(activity, gso)
-            val signInIntent = mGoogleSignInClient.signInIntent
-            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(signInIntent)
-            val account: GoogleSignInAccount = task.getResult(ApiException::class.java)
-            val authCode = account.serverAuthCode
-            viewStateDelegate.switchState(AuthFeature.State.Authenticated(authCode ?: ""))
-        } catch (e: ApiException) {
-            viewStateDelegate.switchState(AuthFeature.State.NetworkError)
-        }
+
+        val mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+        val signInIntent = mGoogleSignInClient.signInIntent
+        signInWithGoogleCallback.launch(signInIntent)
     }
 }
