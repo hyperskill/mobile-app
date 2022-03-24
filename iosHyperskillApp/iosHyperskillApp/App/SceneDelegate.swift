3941b6388b8d9f181e41321109a7304dd962c18b
Ivan Magda <ivan.magda@stepik.org>
2022-03-24 21:15:16 +0700

    Add SceneDelegate

---
+++ iosHyperskillApp/iosHyperskillApp/App/SceneDelegate.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
