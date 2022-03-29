48b22535347d7cc8f8b6f2f98a614f3d65ccb0d2
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2022-03-29 15:33:29 +0300

    Remove ru localization from shared module; Remove fonts from shared module

--- shared/src/commonMain/kotlin/org/hyperskill/app/SharedResourcesFormattedStrings.kt
+++
@@ -1,9 +0,0 @@
-package org.hyperskill.app
-
-import dev.icerock.moko.resources.desc.ResourceFormatted
-import dev.icerock.moko.resources.desc.StringDesc
-
-object SharedResourcesFormattedStrings {
-    fun getFormattedTextExample(placeholder: String): StringDesc =
-        StringDesc.ResourceFormatted(SharedResources.strings.shared_formatted_text_example, placeholder)
-}
