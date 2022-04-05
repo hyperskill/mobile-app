171af45a20aa3d1f3bcc545f814a280ac2d8ef2a
Ivan Magda <ivan.magda@stepik.org>
2022-04-05 18:34:40 +0700

    Add Color extensions

---
+++ iosHyperskillApp/iosHyperskillApp/Theme/Color+DesignSystem.swift
@@ -0,0 +1,74 @@
+import SwiftUI
+import UIKit
+
+extension Color {
+    // MARK: - UI Element Colors -
+
+    // MARK: Text
+
+    /// The material color for text labels that contain primary content.
+    ///
+    /// `OnSurface_0.87`
+    static var primaryText: Color {
+        Color(ColorPalette.onSurfaceAlpha87)
+    }
+
+    /// The material color for text labels that contain secondary content.
+    ///
+    /// `OnSurface_0.6`
+    static var secondaryText: Color {
+        Color(ColorPalette.onSurface.withAlphaComponent(0.6))
+    }
+
+    /// The material color for text labels that contain disabled content.
+    ///
+    /// `OnSurface_0.38`
+    static var disabledText: Color {
+        Color(ColorPalette.onSurface.withAlphaComponent(0.38))
+    }
+
+    /// The system color for text labels that contain primary content.
+    static var systemPrimaryText: Color { Color(UIColor.label) }
+
+    /// The system color for text labels that contain secondary content.
+    static var systemSecondaryText: Color { Color(UIColor.secondaryLabel) }
+
+    /// The system color for text labels that contain tertiary content.
+    static var systemTertiaryText: Color { Color(UIColor.tertiaryLabel) }
+
+    /// The system color for text labels that contain quaternary content.
+    static var systemQuaternaryText: Color { Color(UIColor.quaternaryLabel) }
+
+    /// The system color for placeholder text in controls or text views.
+    static var stepikSystemPlaceholderText: Color { Color(UIColor.placeholderText) }
+
+    // MARK: Separator
+
+    /// The color for thin borders or divider lines that allows some underlying content to be visible.
+    static var separator: Color { Color(UIColor.separator) }
+
+    /// The color for borders or divider lines that hides any underlying content.
+    static var opaqueSeparator: Color { Color(UIColor.opaqueSeparator) }
+
+    // MARK: Standard Content Background Colors
+
+    /// The color for the main background of the interface.
+    static var systemBackground: Color { Color(UIColor.systemBackground) }
+
+    /// The color for content layered on top of the main background.
+    static var systemSecondaryBackground: Color { Color(UIColor.secondarySystemBackground) }
+
+    /// The color for content layered on top of secondary backgrounds.
+    static var systemTertiaryBackground: Color { Color(UIColor.tertiarySystemBackground) }
+
+    // MARK: Grouped Content Background Colors
+
+    /// The color for the main background of grouped interface.
+    static var systemGroupedBackground: Color { Color(UIColor.systemGroupedBackground) }
+
+    /// The color for content layered on top of the main background of grouped interface.
+    static var systemSecondaryGroupedBackground: Color { Color(UIColor.secondarySystemGroupedBackground) }
+
+    /// The color for content layered on top of secondary backgrounds of grouped interface.
+    static var systemTertiaryGroupedBackground: Color { Color(UIColor.tertiarySystemGroupedBackground) }
+}
