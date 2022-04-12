9294897805e91bc9be13090c5cae4fe6d80594c8
Daniil Lyubaev <lyubaevda@gmail.com>
2022-04-12 16:27:00 +0300

    Fix ktlint check

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/auth/presentation/AuthEmailViewModel.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/auth/presentation/AuthEmailViewModel.kt
@@ -4,6 +4,6 @@ import org.hyperskill.app.auth.presentation.AuthFeature
 import ru.nobird.android.view.redux.viewmodel.ReduxViewModel
 import ru.nobird.app.presentation.redux.container.ReduxViewContainer
 
-class AuthEmailViewModel (
+class AuthEmailViewModel(
     reduxViewContainer: ReduxViewContainer<AuthFeature.State, AuthFeature.Message, AuthFeature.Action.ViewAction>
 ) : ReduxViewModel<AuthFeature.State, AuthFeature.Message, AuthFeature.Action.ViewAction>(reduxViewContainer)
