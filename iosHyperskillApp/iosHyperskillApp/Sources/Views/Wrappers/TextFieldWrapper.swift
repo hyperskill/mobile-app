3d323c7a37b1294a0c17bb5ba0d6b3043a35b615
Ivan Magda <ivan.magda@stepik.org>
2022-04-14 13:56:51 +0700

    Add TextFieldWrapper

---
+++ iosHyperskillApp/iosHyperskillApp/Sources/Views/Wrappers/TextFieldWrapper.swift
@@ -0,0 +1,135 @@
+import SwiftUI
+import UIKit
+
+struct TextFieldWrapper: UIViewRepresentable {
+    var placeholder: String?
+
+    @Binding var text: String
+
+    var configuration = Configuration.empty
+
+    var shouldBecomeFirstResponder: Binding<Bool?>?
+
+    var onReturn: (() -> Void)?
+
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField()
+
+        configuration.configure(textField)
+        textField.placeholder = placeholder
+        textField.text = text
+
+        textField.delegate = context.coordinator
+        textField.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.textFieldTextDidChange(_:)),
+            for: .editingChanged
+        )
+
+        return textField
+    }
+
+    func updateUIView(_ textField: UITextField, context: Context) {
+        textField.text = text
+
+        handleShouldBecomeFirstResponder(textField: textField)
+
+        context.coordinator.onTextDidChange = { newText in
+            self.text = newText
+        }
+        context.coordinator.onReturn = {
+            self.shouldBecomeFirstResponder?.wrappedValue = nil
+            self.onReturn?()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    private func handleShouldBecomeFirstResponder(textField: UITextField) {
+        guard let shouldBecomeFirstResponder = shouldBecomeFirstResponder?.wrappedValue else {
+            return
+        }
+
+        DispatchQueue.main.async {
+            if shouldBecomeFirstResponder {
+                textField.becomeFirstResponder()
+            } else {
+                textField.resignFirstResponder()
+            }
+        }
+    }
+}
+
+// MARK: TextFieldWrapper - Coordinator -
+
+extension TextFieldWrapper {
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var onTextDidChange: ((String) -> Void)?
+
+        var onReturn: (() -> Void)?
+
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            onReturn?()
+            return true
+        }
+
+        @objc
+        fileprivate func textFieldTextDidChange(_ textField: UITextField) {
+            onTextDidChange?(textField.text ?? "")
+        }
+    }
+}
+
+// MARK: - TextFieldWrapper (Configuration) -
+
+extension TextFieldWrapper {
+    struct Configuration {
+        var configure: (UITextField) -> Void
+
+        static let empty = Self { _ in }
+
+        static let partOfChain = Self { $0.returnKeyType = .next }
+
+        static let lastOfChainDone = Self { $0.returnKeyType = .done }
+
+        static let lastOfChainGo = Self { $0.returnKeyType = .go }
+
+        static let email = Self {
+            $0.keyboardType = .emailAddress
+            $0.autocorrectionType = .no
+            $0.autocapitalizationType = .none
+            $0.spellCheckingType = .no
+            $0.clearButtonMode = .whileEditing
+        }
+
+        static let password = Self {
+            $0.keyboardType = .default
+            $0.isSecureTextEntry = true
+            $0.spellCheckingType = .no
+        }
+
+        static func combined(_ configurations: [Self]) -> Self {
+            .init { textField in
+                for configuration in configurations {
+                    configuration.configure(textField)
+                }
+            }
+        }
+    }
+}
+
+// MARK: - TextFieldWrapper (Previews) -
+
+struct TextFieldWrapper_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldWrapper(
+            placeholder: "Email",
+            text: .constant(""),
+            configuration: .combined([.email, .partOfChain])
+        )
+        .frame(height: 44)
+        .padding(.horizontal)
+    }
+}
