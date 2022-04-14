7badfb21971694b19b7bbc8049a7c02c1926ba68
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2022-04-14 15:33:20 +0300

    Add DI for ResourceProvider; Add FAB;

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/step/view/ui/fragment/StepFragment.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/step/view/ui/fragment/StepFragment.kt
@@ -9,17 +9,21 @@ import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
-import org.hyperskill.app.SharedResourcesFormatted
+import com.google.android.material.appbar.AppBarLayout
+import org.hyperskill.app.SharedResources
 import org.hyperskill.app.android.HyperskillApp
 import org.hyperskill.app.android.R
 import org.hyperskill.app.android.databinding.FragmentStepTheoryBinding
 import org.hyperskill.app.android.databinding.ItemStepTheoryRatingBinding
 import org.hyperskill.app.android.step.view.model.StepTheoryRating
 import org.hyperskill.app.android.step.view.ui.adapter.decoration.RatingItemDecoration
+import org.hyperskill.app.core.view.mapper.ResourceProvider
 import org.hyperskill.app.step.presentation.StepFeature
 import ru.nobird.android.ui.adapterdelegates.dsl.adapterDelegate
 import ru.nobird.android.ui.adapters.DefaultDelegateAdapter
 import ru.nobird.app.presentation.redux.container.ReduxView
+import javax.inject.Inject
+import kotlin.math.abs
 
 class StepFragment :
     Fragment(R.layout.fragment_step_theory),
@@ -28,6 +32,9 @@ class StepFragment :
         fun newInstance(): StepFragment =
             StepFragment()
     }
+
+    @Inject
+    internal lateinit var resourceProvider: ResourceProvider
 
     private val viewBinding: FragmentStepTheoryBinding by viewBinding(FragmentStepTheoryBinding::bind)
     private val stepTheoryRatingAdapter: DefaultDelegateAdapter<StepTheoryRating> = DefaultDelegateAdapter()
@@ -63,17 +70,29 @@ class StepFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewBinding.stepTheoryTimeToComplete.text = SharedResourcesFormatted.getStepTheoryTimeRemaining(3).toString(requireContext())
+        viewBinding.stepAppBar.addOnOffsetChangedListener(
+            AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+                if (abs(verticalOffset) == appBarLayout.totalScrollRange) {
+                    viewBinding.fab.show()
+                } else {
+                    viewBinding.fab.hide()
+                }
+            }
+        )
+        viewBinding.stepTheoryTimeToComplete.text = resourceProvider.getString(
+            SharedResources.strings.step_theory_reading_text,
+            resourceProvider.getQuantityString(SharedResources.plurals.minutes, 3, 3)
+        )
         viewBinding.stepTheoryContent.text = TEMPLATE_TEXT
         viewBinding.stepTheoryReactionTitle.text = buildSpannedString {
-            append(resources.getString(R.string.step_theory_reaction_subtitle_part_1))
+            append(resources.getString(R.string.step_rating_text_part_1))
             append(" ")
             bold {
-                append(resources.getString(R.string.step_theory_reaction_subtitle_part_2))
+                append(resources.getString(R.string.step_rating_text_part_2))
             }
         }
 
-        ViewCompat.setBackgroundTintList(viewBinding.stepTheoryRatingRecycler, AppCompatResources.getColorStateList(requireContext(), R.color.color_on_surface_alpha_25))
+        ViewCompat.setBackgroundTintList(viewBinding.stepTheoryRatingRecycler, AppCompatResources.getColorStateList(requireContext(), R.color.color_background))
         viewBinding.stepTheoryRatingRecycler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         viewBinding.stepTheoryRatingRecycler.adapter = stepTheoryRatingAdapter
         viewBinding.stepTheoryRatingRecycler.addItemDecoration(
