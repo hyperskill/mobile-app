ac1d00401850e8f473f30815a5067c2747d8a5aa
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2022-04-20 14:07:23 +0300

    Remove redundant class

--- shared/src/commonMain/kotlin/org/hyperskill/app/SharedResourcesFormatted.kt
+++
@@ -1,9 +0,0 @@
-package org.hyperskill.app
-
-import dev.icerock.moko.resources.desc.PluralFormatted
-import dev.icerock.moko.resources.desc.StringDesc
-
-object SharedResourcesFormatted {
-    fun getStepTheoryTimeRemaining(minutes: Int): StringDesc =
-        StringDesc.PluralFormatted(SharedResources.plurals.minutes, minutes, minutes)
-}
