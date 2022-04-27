2b0a56262efd1cc5178647b4ca6fe96a7e1763e0
Rostislav Smirnov <rostislavsmirnoff@gmail.com>
2022-04-27 21:24:44 +0300

    Configure release build variant

--- androidHyperskillApp/build.gradle.kts
+++ androidHyperskillApp/build.gradle.kts
@@ -109,7 +109,9 @@ android {
         }
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
