bae1e6841133a2ea547f6898d842bd3e2186337c
Ivan Magda <ivan.magda@stepik.org>
2022-04-28 11:49:06 +0700

    Migrate from SwiftUI to UIKit Life Cycle

--- iosHyperskillApp/iosHyperskillApp/Sources/App/AppDelegate.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/App/AppDelegate.swift
@@ -1,16 +1,10 @@
 import GoogleSignIn
+import SwiftUI
 import UIKit
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    /// `iosHyperskillApp.AppDelegate` is not direct `UIApplicationDelegate`, it's transferred via `UIApplicationDelegateAdaptor`
-    /// to internal private `SwiftUI.AppDelegate`, which is real `UIApplicationDelegate` and which propagates delegate callbacks.
-    private(set) static var shared: AppDelegate?
-
-    var window: UIWindow? {
-        didSet {
-            ProgressHUD.configure()
-        }
-    }
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
     // MARK: Initializing the App
 
@@ -18,23 +12,17 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
-        Self.shared = self
+        self.window = UIWindow(frame: UIScreen.main.bounds)
 
+        let rootView = AppAssembly().makeModule()
+        let rootViewController = UIHostingController(rootView: rootView)
+        self.window?.rootViewController = rootViewController
+        self.window?.makeKeyAndVisible()
+
+        ProgressHUD.configure()
         KeyboardManager.configure()
 
         return true
-    }
-
-    // MARK: Configuring and Discarding Scenes
-
-    func application(
-        _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
-        let sceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
-        sceneConfiguration.delegateClass = SceneDelegate.self
-        return sceneConfiguration
     }
 
     // MARK: Opening a URL-Specified Resource
--- iosHyperskillApp/iosHyperskillApp/Sources/Systems/ProgressHUD.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Systems/ProgressHUD.swift
@@ -1,53 +1,32 @@
 import SVProgressHUD
 import UIKit
 
+@MainActor
 enum ProgressHUD {
     static func configure() {
-        withSafe {
-            SVProgressHUD.setMinimumDismissTimeInterval(0.5)
-            SVProgressHUD.setDefaultMaskType(SVProgressHUDMaskType.clear)
-            SVProgressHUD.setDefaultStyle(SVProgressHUDStyle.light)
-            SVProgressHUD.setHapticsEnabled(true)
-        }
+        SVProgressHUD.setMinimumDismissTimeInterval(0.5)
+        SVProgressHUD.setDefaultMaskType(SVProgressHUDMaskType.clear)
+        SVProgressHUD.setDefaultStyle(SVProgressHUDStyle.light)
+        SVProgressHUD.setHapticsEnabled(true)
     }
 
     static func updateStyle(isDark: Bool) {
-        withSafe {
-            SVProgressHUD.setDefaultStyle(isDark ? .dark : .light)
-        }
+        SVProgressHUD.setDefaultStyle(isDark ? .dark : .light)
     }
 
     static func show(status: String? = nil) {
-        withSafe {
-            SVProgressHUD.show(withStatus: status)
-        }
+        SVProgressHUD.show(withStatus: status)
     }
 
     static func showError(status: String? = nil) {
-        withSafe {
-            SVProgressHUD.showError(withStatus: status)
-        }
+        SVProgressHUD.showError(withStatus: status)
     }
 
     static func showSuccess(status: String? = nil) {
-        withSafe {
-            SVProgressHUD.showSuccess(withStatus: status)
-        }
+        SVProgressHUD.showSuccess(withStatus: status)
     }
 
     static func dismiss() {
-        withSafe {
-            SVProgressHUD.dismiss()
-        }
-    }
-
-    private static func withSafe(_ block: @escaping () -> Void) {
-        guard UIApplication.shared.delegate?.window?.flatMap({ $0 }) != nil else {
-            return
-        }
-
-        DispatchQueue.main.async {
-            block()
-        }
+        SVProgressHUD.dismiss()
     }
 }
