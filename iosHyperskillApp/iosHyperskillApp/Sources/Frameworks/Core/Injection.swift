92593971e33417ff9518ad2afd682fc1d2029ad1
Ivan Magda <ivan.magda@stepik.org>
2022-06-01 13:54:31 +0700

    iOS: Delete unused code

--- iosHyperskillApp/iosHyperskillApp/Sources/Frameworks/Core/Injection.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Frameworks/Core/Injection.swift
@@ -4,19 +4,3 @@ import shared
 enum AppGraphBridge {
     static let shared: iOSAppComponent = AppGraphImpl(userAgentInfo: UserAgentBuilder.userAgentInfo)
 }
-
-// MARK: Cache
-
-extension Settings {
-    static var `default`: Multiplatform_settingsSettings {
-        Settings.shared.makeAppleSettings(userDefaults: .standard)
-    }
-}
-
-// MARK: Remote
-
-extension AuthDataBuilder {
-    static let sharedAuthorizationFlow = AuthDataBuilder.shared.provideAuthorizationFlow()
-
-    static let sharedAuthorizationMutex = AuthDataBuilder.shared.provideAuthorizationMutex()
-}
