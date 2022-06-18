07ad1851e459911ee386903ecce530752b2b2a32
Daniil Lyubaev <lyubaevda@gmail.com>
2022-06-18 15:07:39 +0300

    Fix wildcard import

--- shared/src/commonMain/kotlin/org/hyperskill/app/profile/remote/ProfileRemoteDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/profile/remote/ProfileRemoteDataSourceImpl.kt
@@ -1,8 +1,9 @@
 package org.hyperskill.app.profile.remote
 
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.request.*
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.header
 import org.hyperskill.app.profile.data.source.ProfileRemoteDataSource
 import org.hyperskill.app.profile.domain.model.Profile
 import org.hyperskill.app.profile.remote.model.ProfileResponse
