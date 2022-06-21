7c95cb365b8a209fe9f78ee301ff713042a09c02
Daniil Lyubaev <lyubaevda@gmail.com>
2022-06-21 11:18:09 +0300

    Refactor ProfileRemoteDataSourceImpl with contentType

--- shared/src/commonMain/kotlin/org/hyperskill/app/profile/remote/ProfileRemoteDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/profile/remote/ProfileRemoteDataSourceImpl.kt
@@ -3,7 +3,7 @@ package org.hyperskill.app.profile.remote
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
-import io.ktor.client.request.header
+import io.ktor.http.*
 import org.hyperskill.app.profile.data.source.ProfileRemoteDataSource
 import org.hyperskill.app.profile.domain.model.Profile
 import org.hyperskill.app.profile.remote.model.ProfileResponse
@@ -15,7 +15,7 @@ class ProfileRemoteDataSourceImpl(
         kotlin.runCatching {
             httpClient
                 .get("/api/profiles/current") {
-                    header("Content-Type", "application/json")
+                    contentType(ContentType.Application.Json)
                 }.body<ProfileResponse>().profiles.first()
         }
 }
