b6a9808dac14992bfd1a3b4185e526b21bb0ccf0
Daniil Lyubaev <lyubaevda@gmail.com>
2022-06-21 11:26:39 +0300

    Fix return type

--- shared/src/commonMain/kotlin/org/hyperskill/app/streak/domain/interactor/StreakInteractor.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/streak/domain/interactor/StreakInteractor.kt
@@ -1,10 +1,11 @@
 package org.hyperskill.app.streak.domain.interactor
 
+import org.hyperskill.app.streak.domain.model.Streak
 import org.hyperskill.app.streak.domain.repository.StreakRepository
 
 class StreakInteractor(
     private val streakRepository: StreakRepository
 ) {
-    suspend fun getStreaks(userId: Long) =
+    suspend fun getStreaks(userId: Long): Result<List<Streak>> =
         streakRepository.getStreaks(userId)
 }
