90e19b73d2c6610ce50af89d49f0680dec6395ec
Daniil Lyubaev <lyubaevda@gmail.com>
2022-06-30 20:18:19 +0300

    Remove unused import

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/home/view/ui/screen/PlaceholderNewUserScreen.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/home/view/ui/screen/PlaceholderNewUserScreen.kt
@@ -4,7 +4,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 import org.hyperskill.app.android.home.view.ui.fragment.PlaceholderNewUserFragment
-import org.hyperskill.app.android.main.view.ui.navigation.Tabs
 
 object PlaceholderNewUserScreen : FragmentScreen {
     override fun createFragment(factory: FragmentFactory): Fragment =
