b018ba4a3953792bc7bcc295dce41d6d0304e1c1
vladkash <spanch-kash@yandex.ru>
2022-08-01 13:59:26 +0500

    create files

---
+++ iosHyperskillApp/iosHyperskillApp/Sources/Frameworks/Notifications/UserNotificationsCenterDelegate.swift
@@ -0,0 +1,19 @@
+import Foundation
+import UserNotifications
+
+final class UserNotificationsCenterDelegate: NSObject {
+    func attachNotificationDelegate() {
+        UNUserNotificationCenter.current().delegate = self
+    }
+}
+
+extension UserNotificationsCenterDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.alert, .sound])
+    }
+}
+Footer
