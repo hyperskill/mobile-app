44c823e8ad6cbc49d037b17e7674421581ed57e3
vladkash <spanch-kash@yandex.ru>
2022-08-02 14:22:56 +0500

    swiftlint fixes

--- shared/src/commonMain/kotlin/org/hyperskill/app/notification/domain/NotificationInteractor.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/notification/domain/NotificationInteractor.kt
@@ -5,6 +5,9 @@ import org.hyperskill.app.notification.domain.repository.NotificationRepository
 class NotificationInteractor(
     private val notificationRepository: NotificationRepository
 ) {
-    suspend fun getNotificationsEnabled() = notificationRepository.getNotificationsEnabled()
-    suspend fun getNotificationsTimestamp() = notificationRepository.getNotificationsTimestamp()
+    suspend fun getNotificationsEnabled(): Boolean =
+        notificationRepository.getNotificationsEnabled()
+
+    suspend fun getNotificationsTimestamp(): Int =
+        notificationRepository.getNotificationsTimestamp()
 }
