da3b0bbf29df7a80313cc94357fd89bdd097e314
Daniil Lyubaev <lyubaevda@gmail.com>
2022-08-03 14:16:13 +0300

    ALTAPPS-205: Set opening app on mock notification click, change title, text and small icon

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/notification/MockNotificationDelegate.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/notification/MockNotificationDelegate.kt
@@ -1,9 +1,13 @@
 package org.hyperskill.app.android.notification
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
+import org.hyperskill.app.android.HyperskillApp
 import org.hyperskill.app.android.R
 import org.hyperskill.app.android.core.extensions.DateTimeHelper
+import org.hyperskill.app.android.main.view.ui.activity.MainActivity
 import org.hyperskill.app.android.notification.model.HyperskillNotificationChannel
 
 // TODO remove before merging to develop
@@ -12,10 +16,14 @@ class MockNotificationDelegate(
     hyperskillNotificationManager: HyperskillNotificationManager
 ) : NotificationDelegate("mock_notification", hyperskillNotificationManager) {
     override fun onNeedShowNotification() {
+        val intent = Intent(HyperskillApp.getAppContext(), MainActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(HyperskillApp.getAppContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
         val notification = NotificationCompat.Builder(context, HyperskillNotificationChannel.other.channelId)
-            .setContentTitle("Mock title")
-            .setContentText("Mock content")
-            .setSmallIcon(R.drawable.ic_close)
+            .setContentTitle("Hyperskill Mock Notification")
+            .setContentText("Tap to open the app!")
+            .setSmallIcon(R.drawable.ic_branded_logo_splash)
+            .setContentIntent(pendingIntent)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
 
         showNotification(0, notification.build())
