87bafa54eff0fc3fbfdeb26763deb8db110b9631
vladkash <spanch-kash@yandex.ru>
2022-08-07 16:49:08 +0500

    ALTAPPS-206: compilation fixes

--- shared/src/commonMain/kotlin/org/hyperskill/app/profile_settings/injection/ProfileSettingsComponentImpl.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/profile_settings/injection/ProfileSettingsComponentImpl.kt
@@ -1,6 +1,20 @@
 package org.hyperskill.app.profile_settings.injection
 
+import org.hyperskill.app.auth.cache.AuthCacheDataSourceImpl
+import org.hyperskill.app.auth.data.repository.AuthRepositoryImpl
+import org.hyperskill.app.auth.data.source.AuthCacheDataSource
+import org.hyperskill.app.auth.data.source.AuthRemoteDataSource
+import org.hyperskill.app.auth.domain.interactor.AuthInteractor
+import org.hyperskill.app.auth.domain.repository.AuthRepository
+import org.hyperskill.app.auth.remote.source.AuthRemoteDataSourceImpl
 import org.hyperskill.app.core.injection.AppGraph
+import org.hyperskill.app.profile.cache.ProfileCacheDataSourceImpl
+import org.hyperskill.app.profile.data.repository.ProfileRepositoryImpl
+import org.hyperskill.app.profile.data.source.ProfileCacheDataSource
+import org.hyperskill.app.profile.data.source.ProfileRemoteDataSource
+import org.hyperskill.app.profile.domain.interactor.ProfileInteractor
+import org.hyperskill.app.profile.domain.repository.ProfileRepository
+import org.hyperskill.app.profile.remote.ProfileRemoteDataSourceImpl
 import org.hyperskill.app.profile_settings.presentation.ProfileSettingsFeature
 import org.hyperskill.app.profile_settings.cache.ProfileSettingsCacheDataSourceImpl
 import org.hyperskill.app.profile_settings.data.repository.ProfileSettingsRepositoryImpl
@@ -21,6 +35,35 @@ class ProfileSettingsComponentImpl(private val appGraph: AppGraph) : ProfileSett
     private val profileSettingsInteractor: ProfileSettingsInteractor =
         ProfileSettingsInteractor(profileSettingsRepository)
 
+    private val authCacheDataSource: AuthCacheDataSource =
+        AuthCacheDataSourceImpl(appGraph.commonComponent.settings)
+    private val authRemoteDataSource: AuthRemoteDataSource = AuthRemoteDataSourceImpl(
+        appGraph.networkComponent.authMutex,
+        appGraph.networkComponent.authorizationFlow,
+        appGraph.networkComponent.authSocialHttpClient,
+        appGraph.networkComponent.authCredentialsHttpClient,
+        appGraph.commonComponent.json,
+        appGraph.commonComponent.settings
+    )
+
+    private val authRepository: AuthRepository = AuthRepositoryImpl(
+        authCacheDataSource,
+        authRemoteDataSource
+    )
+    private val authInteractor: AuthInteractor =
+        AuthInteractor(authRepository)
+
+    private val profileRemoteDataSource: ProfileRemoteDataSource = ProfileRemoteDataSourceImpl(
+        appGraph.networkComponent.authorizedHttpClient
+    )
+    private val profileCacheDataSource: ProfileCacheDataSource = ProfileCacheDataSourceImpl(
+        appGraph.commonComponent.json,
+        appGraph.commonComponent.settings
+    )
+    private val profileRepository: ProfileRepository =
+        ProfileRepositoryImpl(profileRemoteDataSource, profileCacheDataSource)
+    private val profileInteractor: ProfileInteractor = ProfileInteractor(profileRepository)
+
     override val profileSettingsFeature: Feature<ProfileSettingsFeature.State, ProfileSettingsFeature.Message, ProfileSettingsFeature.Action>
-        get() = ProfileSettingsFeatureBuilder.build(profileSettingsInteractor)
+        get() = ProfileSettingsFeatureBuilder.build(profileSettingsInteractor, authInteractor, profileInteractor)
 }
--- shared/src/commonMain/kotlin/org/hyperskill/app/profile_settings/injection/ProfileSettingsFeatureBuilder.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/profile_settings/injection/ProfileSettingsFeatureBuilder.kt
@@ -1,6 +1,8 @@
 package org.hyperskill.app.profile_settings.injection
 
+import org.hyperskill.app.auth.domain.interactor.AuthInteractor
 import org.hyperskill.app.core.presentation.ActionDispatcherOptions
+import org.hyperskill.app.profile.domain.interactor.ProfileInteractor
 import org.hyperskill.app.profile_settings.presentation.ProfileSettingsActionDispatcher
 import org.hyperskill.app.profile_settings.presentation.ProfileSettingsFeature
 import org.hyperskill.app.profile_settings.presentation.ProfileSettingsReducer
@@ -10,10 +12,10 @@ import ru.nobird.app.presentation.redux.feature.Feature
 import ru.nobird.app.presentation.redux.feature.ReduxFeature
 
 object ProfileSettingsFeatureBuilder {
-    fun build(profileSettingsInteractor: ProfileSettingsInteractor): Feature<ProfileSettingsFeature.State, ProfileSettingsFeature.Message, ProfileSettingsFeature.Action> {
+    fun build(profileSettingsInteractor: ProfileSettingsInteractor, authInteractor: AuthInteractor, profileInteractor: ProfileInteractor): Feature<ProfileSettingsFeature.State, ProfileSettingsFeature.Message, ProfileSettingsFeature.Action> {
         val profileSettingsReducer = ProfileSettingsReducer()
         val profileSettingsActionDispatcher =
-            ProfileSettingsActionDispatcher(ActionDispatcherOptions(), profileSettingsInteractor)
+            ProfileSettingsActionDispatcher(ActionDispatcherOptions(), profileSettingsInteractor, authInteractor, profileInteractor)
 
         return ReduxFeature(ProfileSettingsFeature.State.Idle, profileSettingsReducer)
             .wrapWithActionDispatcher(profileSettingsActionDispatcher)
