0d44fcbc626e3419e0d2ff35aed4ee1f260d3156
Daniil Lyubaev <lyubaevda@gmail.com>
2022-08-18 12:07:04 +0300

    ALTAPPS-228: Add PR fixes

--- shared/src/commonMain/kotlin/org/hyperskill/app/home/presentation/HomeReducer.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/home/presentation/HomeReducer.kt
@@ -48,7 +48,18 @@ class HomeReducer : StateReducer<State, Message, Action> {
                     state.problemOfDayState is HomeFeature.ProblemOfDayState.NeedToSolve &&
                     state.problemOfDayState.step.id == message.stepId
                 ) {
-                    state to setOf(Action.UpdateOnProblemOfDaySolved(state.streak))
+                    val completedStep = state.problemOfDayState.step.copy(
+                        isCompleted = true
+                    )
+                    val updatedStreak = state.streak?.getStreakWithTodaySolved()
+
+                    State.Content(
+                        updatedStreak,
+                        HomeFeature.ProblemOfDayState.Solved(
+                            completedStep,
+                            HomeActionDispatcher.calculateNextProblemIn()
+                        )
+                    ) to setOf()
                 } else {
                     null
                 }
