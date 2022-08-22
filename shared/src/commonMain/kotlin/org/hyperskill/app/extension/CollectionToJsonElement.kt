4e44a05cf2500740c9212e04b4a0872989d1c156
Ivan Magda <ivan.magda@stepik.org>
2022-08-22 23:55:18 +0400

    ALTAPPS-223: fix ktlint

--- shared/src/commonMain/kotlin/org/hyperskill/app/extension/CollectionToJsonElement.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/extension/CollectionToJsonElement.kt
@@ -6,20 +6,23 @@ import kotlinx.serialization.json.JsonNull
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
 
-fun Collection<*>.toJsonElement(): JsonElement = JsonArray(mapNotNull { it.toJsonElement() })
+fun Collection<*>.toJsonElement(): JsonElement =
+    JsonArray(mapNotNull { it.toJsonElement() })
 
-fun Map<*, *>.toJsonElement(): JsonElement = JsonObject(
-    mapNotNull {
-        (it.key as? String ?: return@mapNotNull null) to it.value.toJsonElement()
-    }.toMap(),
-)
+fun Map<*, *>.toJsonElement(): JsonElement =
+    JsonObject(
+        mapNotNull {
+            (it.key as? String ?: return@mapNotNull null) to it.value.toJsonElement()
+        }.toMap(),
+    )
 
-fun Any?.toJsonElement(): JsonElement = when (this) {
-    null -> JsonNull
-    is Map<*, *> -> toJsonElement()
-    is Collection<*> -> toJsonElement()
-    is Boolean -> JsonPrimitive(this)
-    is Number -> JsonPrimitive(this)
-    is String -> JsonPrimitive(this)
-    else -> throw IllegalStateException("Can't serialize unknown type: $this")
-}
+fun Any?.toJsonElement(): JsonElement =
+    when (this) {
+        null -> JsonNull
+        is Map<*, *> -> toJsonElement()
+        is Collection<*> -> toJsonElement()
+        is Boolean -> JsonPrimitive(this)
+        is Number -> JsonPrimitive(this)
+        is String -> JsonPrimitive(this)
+        else -> throw IllegalStateException("Can't serialize unknown type: $this")
+    }
