37d8aa9368bfba118b870a320811c25b4e08c2c3
vladkash <spanch-kash@yandex.ru>
2022-08-25 13:16:21 +0500

    ALTAPPS-255: iOS reload home screen after launching app from background

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/Home/HomeViewModel.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/Home/HomeViewModel.swift
@@ -1,13 +1,48 @@
 import Foundation
 import shared
+import UIKit
 
 final class HomeViewModel: FeatureViewModel<HomeFeatureState, HomeFeatureMessage, HomeFeatureActionViewAction> {
+    private var wasInBackground = false
+
+    override init(feature: Presentation_reduxFeature) {
+        super.init(feature: feature)
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleApplicationDidBecomeActive(_:)),
+            name: UIApplication.didBecomeActiveNotification,
+            object: UIApplication.shared
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleApplicationDidEnterBackground(_:)),
+            name: UIApplication.didEnterBackgroundNotification,
+            object: UIApplication.shared
+        )
+    }
+
     func loadContent(forceUpdate: Bool = false) {
         onNewMessage(HomeFeatureMessageInit(forceUpdate: forceUpdate))
     }
 
     func logViewedEvent() {
         onNewMessage(HomeFeatureMessageHomeViewedEventMessage())
+    }
+
+    // MARK: Private API
+
+    @objc
+    private func handleApplicationDidBecomeActive(_: NSNotification) {
+        if wasInBackground {
+            wasInBackground = false
+            onNewMessage(HomeFeatureMessageInit(forceUpdate: true))
+        }
+    }
+
+    @objc
+    private func handleApplicationDidEnterBackground(_: NSNotification) {
+        wasInBackground = true
     }
 }
 
