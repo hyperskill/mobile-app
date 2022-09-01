4fdb1e36244c958c6f528498ae9b7b6a277e9a17
Ivan Magda <ivan.magda@stepik.org>
2022-09-01 23:06:54 +0400

    ALTAPPS-265: Fix build

--- shared/src/commonMain/kotlin/org/hyperskill/app/profile_settings/injection/ProfileSettingsComponentImpl.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/profile_settings/injection/ProfileSettingsComponentImpl.kt
@@ -2,6 +2,7 @@ package org.hyperskill.app.profile_settings.injection
 
 import org.hyperskill.app.config.BuildKonfig
 import org.hyperskill.app.core.injection.AppGraph
+import org.hyperskill.app.profile.domain.interactor.ProfileInteractor
 import org.hyperskill.app.profile_settings.cache.ProfileSettingsCacheDataSourceImpl
 import org.hyperskill.app.profile_settings.data.repository.ProfileSettingsRepositoryImpl
 import org.hyperskill.app.profile_settings.data.source.ProfileSettingsCacheDataSource
@@ -21,10 +22,13 @@ class ProfileSettingsComponentImpl(private val appGraph: AppGraph) : ProfileSett
     override val profileSettingsInteractor: ProfileSettingsInteractor =
         ProfileSettingsInteractor(profileSettingsRepository)
 
+    private val profileInteractor: ProfileInteractor =
+        appGraph.buildProfileDataComponent().profileInteractor
+
     override val profileSettingsFeature: Feature<ProfileSettingsFeature.State, ProfileSettingsFeature.Message, ProfileSettingsFeature.Action>
         get() = ProfileSettingsFeatureBuilder.build(
             profileSettingsInteractor,
-            appGraph.buildProfileDataComponent().profileInteractor,
+            profileInteractor,
             appGraph.analyticComponent.analyticInteractor,
             appGraph.networkComponent.authorizationFlow
         )
