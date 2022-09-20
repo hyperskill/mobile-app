441ad7467299d83bc033aeae78f63c1bb3269cc8
Ivan Magda <ivan.magda@stepik.org>
2022-09-20 12:28:53 +0300

    Android: Add home open step input layout

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/home/view/ui/fragment/HomeFragment.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/home/view/ui/fragment/HomeFragment.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.snackbar.Snackbar
 import org.hyperskill.app.android.HyperskillApp
 import org.hyperskill.app.android.R
 import org.hyperskill.app.android.core.view.ui.navigation.requireRouter
@@ -16,6 +17,7 @@ import org.hyperskill.app.android.databinding.FragmentHomeBinding
 import org.hyperskill.app.android.problem_of_day.view.delegate.ProblemOfDayCardFormDelegate
 import org.hyperskill.app.android.step.view.screen.StepScreen
 import org.hyperskill.app.android.streak.view.delegate.StreakCardFormDelegate
+import org.hyperskill.app.android.view.base.ui.extension.snackbar
 import org.hyperskill.app.config.BuildKonfig
 import org.hyperskill.app.home.presentation.HomeFeature
 import org.hyperskill.app.home.presentation.HomeViewModel
@@ -69,6 +71,15 @@ class HomeFragment :
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(BuildKonfig.BASE_URL)
             startActivity(intent)
+        }
+
+        viewBinding.homeOpenStepButton.setOnClickListener {
+            val stepId = viewBinding.homeOpenStepInputEditText.text.toString().toLongOrNull()
+            if (stepId == null) {
+                view.snackbar("Insert a valid number", Snackbar.LENGTH_SHORT)
+            } else {
+                requireRouter().navigateTo(StepScreen(stepId))
+            }
         }
 
         homeViewModel.onNewMessage(HomeFeature.Message.Init(forceUpdate = false))
