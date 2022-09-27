531b15f9e553ea4f9db5fa59813306b731331e20
vladkash <spanch-kash@yandex.ru>
2022-09-27 15:20:29 +0500

    ALTAPPS-301: shared navigate to onboarding after logout

--- shared/src/commonMain/kotlin/org/hyperskill/app/main/presentation/AppReducer.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/main/presentation/AppReducer.kt
@@ -31,7 +31,7 @@ class AppReducer : StateReducer<State, Message, Action> {
                 }
             is Message.UserDeauthorized ->
                 if (state is State.Ready && state.isAuthorized) {
-                    State.Ready(isAuthorized = false) to setOf(Action.ViewAction.NavigateTo.AuthScreen)
+                    State.Ready(isAuthorized = false) to setOf(Action.ViewAction.NavigateTo.OnboardingScreen)
                 } else {
                     null
                 }
