8d52f6002d6248e0d5fb5ab5fb02a2d07c3e0ba5
Ivan Magda <ivan.magda@stepik.org>
2022-10-03 18:54:10 +0400

    ALTAPPS-323: Add SentryBreadcrumbKeyValues

---
+++ shared/src/commonMain/kotlin/org/hyperskill/app/sentry/domain/model/SentryBreadcrumbKeyValues.kt
@@ -0,0 +1,6 @@
+package org.hyperskill.app.sentry.domain.model
+
+object SentryBreadcrumbKeyValues {
+    const val CATEGORY_AUTH_SOCIAL = "auth_social"
+    const val CATEGORY_AUTH_CREDENTIALS = "auth_social_credentials"
+}
