e451de63a353137a7c8848115c6534885272de94
vladkash <spanch-kash@yandex.ru>
2022-10-04 16:51:10 +0500

    ALTAPPS-313: review fixes

--- shared/src/commonMain/kotlin/org/hyperskill/app/user_storage/data/repository/UserStorageRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/user_storage/data/repository/UserStorageRepositoryImpl.kt
@@ -4,6 +4,7 @@ import kotlinx.serialization.json.jsonObject
 import org.hyperskill.app.core.domain.DataSourceType
 import org.hyperskill.app.user_storage.data.source.UserStorageCacheDataSource
 import org.hyperskill.app.user_storage.data.source.UserStorageRemoteDataSource
+import org.hyperskill.app.user_storage.domain.model.UserStorage
 import org.hyperskill.app.user_storage.domain.model.UserStorageKey
 import org.hyperskill.app.user_storage.domain.model.UserStorageValue
 import org.hyperskill.app.user_storage.domain.repository.UserStorageRepository
@@ -29,11 +30,7 @@ class UserStorageRepositoryImpl(
                     .getUserStorage()
         }
 
-        return key.split(".").fold(userStorage as Result<UserStorageValue>) { acc, s ->
-            acc.getOrNull()?.jsonObject?.get(s)?.let { Result.success(it) } ?: Result.failure(
-                NoSuchElementException()
-            )
-        }
+        return getUserStorageValueByCompositeKey(key, userStorage)
     }
 
     override suspend fun updateUserStorage(key: UserStorageKey, value: UserStorageValue) {
@@ -43,4 +40,11 @@ class UserStorageRepositoryImpl(
     override fun clearCache() {
         userStorageCacheDataSource.clearCache()
     }
+
+    private fun getUserStorageValueByCompositeKey(key: UserStorageKey, userStorage: Result<UserStorage>) =
+        key.split(".").fold(userStorage as Result<UserStorageValue>) { jsonElementResult, keyPart ->
+            jsonElementResult.getOrNull()?.jsonObject?.get(keyPart)?.let { Result.success(it) } ?: Result.failure(
+                NoSuchElementException()
+            )
+        }
 }
