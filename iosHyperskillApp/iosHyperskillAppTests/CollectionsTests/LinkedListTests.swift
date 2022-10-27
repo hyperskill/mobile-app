6d812dd018874a0160ef42ae1608c5f540778249
Ivan Magda <ivan.magda@stepik.org>
2022-10-27 17:33:10 +0400

    ALTAPPS-377: Add setup ios action

--- iosHyperskillApp/iosHyperskillAppTests/CollectionsTests/LinkedListTests.swift
+++ iosHyperskillApp/iosHyperskillAppTests/CollectionsTests/LinkedListTests.swift
@@ -15,6 +15,7 @@ class LinkedListTest: XCTestCase {
     }
 
     func testEmptyList() {
+        print("TEST")
         let list = LinkedList<Int>()
         XCTAssertTrue(list.isEmpty)
         XCTAssertNil(list.head)
