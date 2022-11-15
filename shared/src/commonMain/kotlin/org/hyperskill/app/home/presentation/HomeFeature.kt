0d4cbbdba42dfe97f285745288d156f3c867c13f
vladkash <spanch-kash@yandex.ru>
2022-11-15 17:01:42 +0500

    ALTAPPS-394: fix ktlint

--- shared/src/commonMain/kotlin/org/hyperskill/app/home/presentation/HomeFeature.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/home/presentation/HomeFeature.kt
@@ -43,7 +43,7 @@ interface HomeFeature {
         sealed interface ViewAction : Action {
             sealed interface NavigateTo : ViewAction {
                 data class StepScreen(val stepId: Long) : NavigateTo
-                object TopicsRepetitions: NavigateTo
+                object TopicsRepetitions : NavigateTo
             }
         }
     }
