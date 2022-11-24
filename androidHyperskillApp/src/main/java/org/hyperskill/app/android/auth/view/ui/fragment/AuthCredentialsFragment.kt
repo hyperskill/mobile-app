8769592c9ae07e2131a1d1cc308332ab3c5348ef
XanderZhu <s.p.q.r.4century@gmail.com>
2022-11-24 14:19:56 +0400

    ALTAPPS-390: fix stored dialogFragment references in parent fragment

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/auth/view/ui/fragment/AuthCredentialsFragment.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/auth/view/ui/fragment/AuthCredentialsFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.inputmethod.EditorInfo
 import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -20,7 +19,7 @@ import org.hyperskill.app.android.auth.view.ui.navigation.AuthFlow
 import org.hyperskill.app.android.auth.view.ui.navigation.AuthSocialScreen
 import org.hyperskill.app.android.core.extensions.launchUrl
 import org.hyperskill.app.android.core.view.ui.dialog.LoadingProgressDialogFragment
-import org.hyperskill.app.android.core.view.ui.dialog.dismissIfExists
+import org.hyperskill.app.android.core.view.ui.dialog.dismissDialogFragmentIfExists
 import org.hyperskill.app.android.core.view.ui.navigation.requireRouter
 import org.hyperskill.app.android.databinding.FragmentAuthEmailBinding
 import org.hyperskill.app.android.sentry.domain.model.SentryBreadcrumbKeyValues
@@ -50,9 +49,6 @@ class AuthCredentialsFragment :
     private val viewStateDelegate: ViewStateDelegate<AuthCredentialsFeature.FormState> = ViewStateDelegate()
     private val authCredentialsViewModel: AuthCredentialsViewModel by reduxViewModel(this) { viewModelFactory }
     private val viewBinding by viewBinding(FragmentAuthEmailBinding::bind)
-
-    private val loadingProgressDialogFragment: DialogFragment =
-        LoadingProgressDialogFragment.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -161,9 +157,10 @@ class AuthCredentialsFragment :
     ) {
         viewStateDelegate.switchState(formState)
         if (formState is AuthCredentialsFeature.FormState.Loading || isLoadingMagicLink) {
-            loadingProgressDialogFragment.showIfNotExists(childFragmentManager, LoadingProgressDialogFragment.TAG)
+            LoadingProgressDialogFragment.newInstance()
+                .showIfNotExists(childFragmentManager, LoadingProgressDialogFragment.TAG)
         } else {
-            loadingProgressDialogFragment.dismissIfExists(childFragmentManager, LoadingProgressDialogFragment.TAG)
+            childFragmentManager.dismissDialogFragmentIfExists(LoadingProgressDialogFragment.TAG)
         }
 
         if (formState is AuthCredentialsFeature.FormState.Error) {
