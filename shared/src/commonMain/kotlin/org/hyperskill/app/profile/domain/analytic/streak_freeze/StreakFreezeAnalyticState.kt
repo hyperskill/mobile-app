81469bc76d02d75a92e14267d131ccf5d2ad38ef
Ivan Magda <ivan.magda@stepik.org>
2022-12-22 15:06:28 +0700

    ALTAPPS-478: Fix not_enough_gems state name

--- shared/src/commonMain/kotlin/org/hyperskill/app/profile/domain/analytic/streak_freeze/StreakFreezeAnalyticState.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/profile/domain/analytic/streak_freeze/StreakFreezeAnalyticState.kt
@@ -2,6 +2,6 @@ package org.hyperskill.app.profile.domain.analytic.streak_freeze
 
 enum class StreakFreezeAnalyticState(val stringValue: String) {
     CAN_BUY("can_buy"),
-    NOT_ENOUGH_GEMS("not_enough_games"),
+    NOT_ENOUGH_GEMS("not_enough_gems"),
     ALREADY_HAVE("already_have")
 }
