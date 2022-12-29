b5be696d8e9d19b79bbab2a640cb15557ccb1ef4
Ivan Magda <ivan.magda@stepik.org>
2022-12-29 11:16:00 +0700

    Reformat code

--- shared/src/commonMain/kotlin/org/hyperskill/app/config/BuildKonfig.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/config/BuildKonfig.kt
@@ -5,63 +5,63 @@ import org.hyperskill.app.debug.domain.model.EndpointConfigType
 
 class BuildKonfig(
     val buildVariant: BuildVariant,
-    private val endpointConfigType: EndpointConfigType
+    endpointConfigType: EndpointConfigType
 ) {
     companion object {
         val IS_DEBUG_CONTROLS_ENABLED: Boolean? = InternalBuildKonfig.IS_DEBUG_CONTROLS_ENABLED
     }
 
-    val flavor: String
-        get() = when (endpointConfigType) {
+    val flavor: String =
+        when (endpointConfigType) {
             EndpointConfigType.DEV -> InternalBuildKonfig.DEV_FLAVOR
             EndpointConfigType.PRODUCTION -> InternalBuildKonfig.PRODUCTION_FLAVOR
             EndpointConfigType.RELEASE -> InternalBuildKonfig.RELEASE_FLAVOR
         }
 
-    internal val baseUrl: String
-        get() = when (endpointConfigType) {
+    internal val baseUrl: String =
+        when (endpointConfigType) {
             EndpointConfigType.DEV -> InternalBuildKonfig.DEV_BASE_URL
             EndpointConfigType.PRODUCTION -> InternalBuildKonfig.PRODUCTION_BASE_URL
             EndpointConfigType.RELEASE -> InternalBuildKonfig.RELEASE_BASE_URL
         }
 
-    internal val host: String
-        get() = when (endpointConfigType) {
+    internal val host: String =
+        when (endpointConfigType) {
             EndpointConfigType.DEV -> InternalBuildKonfig.DEV_HOST
             EndpointConfigType.PRODUCTION -> InternalBuildKonfig.PRODUCTION_HOST
             EndpointConfigType.RELEASE -> InternalBuildKonfig.RELEASE_HOST
         }
 
-    internal val oauthClientId: String
-        get() = when (endpointConfigType) {
+    internal val oauthClientId: String =
+        when (endpointConfigType) {
             EndpointConfigType.DEV -> InternalBuildKonfig.DEV_OAUTH_CLIENT_ID
             EndpointConfigType.PRODUCTION -> InternalBuildKonfig.PRODUCTION_OAUTH_CLIENT_ID
             EndpointConfigType.RELEASE -> InternalBuildKonfig.RELEASE_OAUTH_CLIENT_ID
         }
 
-    internal val oauthClientSecret: String
-        get() = when (endpointConfigType) {
+    internal val oauthClientSecret: String =
+        when (endpointConfigType) {
             EndpointConfigType.DEV -> InternalBuildKonfig.DEV_OAUTH_CLIENT_SECRET
             EndpointConfigType.PRODUCTION -> InternalBuildKonfig.PRODUCTION_OAUTH_CLIENT_SECRET
             EndpointConfigType.RELEASE -> InternalBuildKonfig.RELEASE_OAUTH_CLIENT_SECRET
         }
 
-    internal val redirectUri: String
-        get() = when (endpointConfigType) {
+    internal val redirectUri: String =
+        when (endpointConfigType) {
             EndpointConfigType.DEV -> InternalBuildKonfig.DEV_REDIRECT_URI
             EndpointConfigType.PRODUCTION -> InternalBuildKonfig.PRODUCTION_REDIRECT_URI
             EndpointConfigType.RELEASE -> InternalBuildKonfig.RELEASE_REDIRECT_URI
         }
 
-    internal val credentialsClientId: String
-        get() = when (endpointConfigType) {
+    internal val credentialsClientId: String =
+        when (endpointConfigType) {
             EndpointConfigType.DEV -> InternalBuildKonfig.DEV_CREDENTIALS_CLIENT_ID
             EndpointConfigType.PRODUCTION -> InternalBuildKonfig.PRODUCTION_CREDENTIALS_CLIENT_ID
             EndpointConfigType.RELEASE -> InternalBuildKonfig.RELEASE_CREDENTIALS_CLIENT_ID
         }
 
-    internal val credentialsClientSecret: String
-        get() = when (endpointConfigType) {
+    internal val credentialsClientSecret: String =
+        when (endpointConfigType) {
             EndpointConfigType.DEV -> InternalBuildKonfig.DEV_CREDENTIALS_CLIENT_SECRET
             EndpointConfigType.PRODUCTION -> InternalBuildKonfig.PRODUCTION_CREDENTIALS_CLIENT_SECRET
             EndpointConfigType.RELEASE -> InternalBuildKonfig.RELEASE_CREDENTIALS_CLIENT_SECRET
