1f8052605eb4412490046d64e35f32070b42e095
XanderZhu <s.p.q.r.4century@gmail.com>
2023-02-02 16:54:07 +0400

    Implement "Learn next" badge

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/topics/view/delegate/TopicsToDiscoverNextDelegate.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/topics/view/delegate/TopicsToDiscoverNextDelegate.kt
@@ -1,10 +1,13 @@
 package org.hyperskill.app.android.topics.view.delegate
 
 import android.content.Context
+import android.view.LayoutInflater
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.hyperskill.app.android.R
+import org.hyperskill.app.android.core.view.ui.adapter.decoration.HorizontalMarginItemDecoration
 import org.hyperskill.app.android.core.view.ui.adapter.decoration.VerticalMarginItemDecoration
+import org.hyperskill.app.android.topics.view.adapter_delegate.LearnNextRecyclerItemDecoration
 import org.hyperskill.app.android.topics.view.adapter_delegate.TopicAdapterDelegate
 import org.hyperskill.app.android.topics.view.model.TopicListItem
 import org.hyperskill.app.topics_to_discover_next.presentation.TopicsToDiscoverNextFeature
@@ -29,6 +32,17 @@ class TopicsToDiscoverNextDelegate(
             isNestedScrollingEnabled = false
             addItemDecoration(
                 VerticalMarginItemDecoration(resources.getDimensionPixelSize(R.dimen.track_next_topic_vertical_item_margin))
+            )
+            val horizontalMargin = resources.getDimensionPixelSize(R.dimen.track_next_topic_horizontal_item_margin)
+            addItemDecoration(
+                HorizontalMarginItemDecoration(horizontalMargin)
+            )
+            addItemDecoration(
+                LearnNextRecyclerItemDecoration(
+                    badge = LayoutInflater.from(context).inflate(R.layout.layout_learn_next_topic_badge, recyclerView, false),
+                    marginStart = horizontalMargin +
+                        context.resources.getDimensionPixelOffset(R.dimen.track_next_topic_horizontal_item_padding)
+                )
             )
         }
     }
