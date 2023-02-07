70b44dfd5108c6be98f82ff82d84503ed5ca6126
vladkash <spanch-kash@yandex.ru>
2023-02-07 17:40:20 +0500

    Show line numbers in step code

---
+++ androidHyperskillApp/src/main/assets/scripts/lines_wrapper.js
@@ -0,0 +1,9 @@
+addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('pre code')
+        .forEach(codeBlock => {
+            codeBlock.innerHTML = codeBlock.innerHTML
+                .split(/\r?\n/)
+                .map(line => `<span>${line}</span>`)
+                .join('\n')
+        })
+});
