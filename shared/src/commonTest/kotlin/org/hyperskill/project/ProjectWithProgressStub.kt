ebeeb970483ae364c8ce5d95936cfb9462af2650
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2023-05-11 14:14:05 +0400

    Fix ktlint

--- shared/src/commonTest/kotlin/org/hyperskill/project/ProjectWithProgressStub.kt
+++ shared/src/commonTest/kotlin/org/hyperskill/project/ProjectWithProgressStub.kt
@@ -6,7 +6,8 @@ import org.hyperskill.app.projects.domain.model.ProjectWithProgress
 
 fun ProjectWithProgress.Companion.stub(
     projectId: Long = 0L
-): ProjectWithProgress = ProjectWithProgress(
-    project = Project.stub(projectId),
-    progress = ProjectProgress.stub(projectId)
-)
+): ProjectWithProgress =
+    ProjectWithProgress(
+        project = Project.stub(projectId),
+        progress = ProjectProgress.stub(projectId)
+    )
