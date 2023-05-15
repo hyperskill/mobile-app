ca39fc5ae2a0738eccdae54012366c8d53070bf0
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2023-05-15 13:52:36 +0400

    Implement base ProjectSelectionListFragment & platform injection

---
+++ shared/src/androidMain/kotlin/org/hyperskill/app/project_selection/injection/PlatformProjectSelectionListComponent.kt
@@ -0,0 +1,7 @@
+package org.hyperskill.app.project_selection.injection
+
+import org.hyperskill.app.core.injection.ReduxViewModelFactory
+
+interface PlatformProjectSelectionListComponent {
+    val reduxViewModelFactory: ReduxViewModelFactory
+}
