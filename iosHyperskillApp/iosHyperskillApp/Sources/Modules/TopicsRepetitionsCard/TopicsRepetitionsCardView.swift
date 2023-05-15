500160012254e38339fc9a75e95168898e0f61ad
Ivan Magda <ivan.magda@stepik.org>
2023-05-15 18:45:47 +0300

    Add TrackSelectionListGridCellBadgesView

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/TopicsRepetitionsCard/TopicsRepetitionsCardView.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/TopicsRepetitionsCard/TopicsRepetitionsCardView.swift
@@ -42,7 +42,7 @@ struct TopicsRepetitionsCardView: View {
                     state.buildFooter(topicsToRepeatCount: topicsToRepeatCount)
 
                     if isFreemiumEnabled && state == .uncompleted {
-                        BadgeView.freemiumRepeatUnlimited()
+                        BadgeView.repeatUnlimited()
                     }
                 }
                 .padding()
---
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/TrackSelectionList/Views/Grid/Cell/TrackSelectionListGridCellView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct TrackSelectionListGridCellView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct TrackSelectionListGridCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackSelectionListGridCellView()
+    }
+}
