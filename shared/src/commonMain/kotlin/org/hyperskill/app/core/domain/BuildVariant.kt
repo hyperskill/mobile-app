55598ea3dc4483a67ddc2ab9f0f4d45285caacee
Ivan Magda <ivan.magda@stepik.org>
2023-06-23 17:06:53 +0900

    iOS Sentry add internalRelease environment (#544)
    ^ALTAPPS-843

--- shared/src/commonMain/kotlin/org/hyperskill/app/core/domain/BuildVariant.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/core/domain/BuildVariant.kt
@@ -1,12 +1,20 @@
 package org.hyperskill.app.core.domain
 
-enum class BuildVariant {
-    DEBUG,
-    RELEASE;
+enum class BuildVariant(val value: String) {
+    DEBUG("debug"),
+    RELEASE("release"),
+    INTERNAL_RELEASE("internalRelease");
+
+    companion object {
+        private val VALUES: Array<BuildVariant> = values()
+
+        fun getByValue(value: String): BuildVariant? =
+            VALUES.firstOrNull { it.value == value }
+    }
 
     fun isDebug(): Boolean =
         when (this) {
             DEBUG -> true
-            RELEASE -> false
+            RELEASE, INTERNAL_RELEASE -> false
         }
 }
