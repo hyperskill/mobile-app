ed0115aebf0e94f56b2ab44e83a39901f0879e29
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2023-07-20 13:27:34 +0400

    Rollout progress screen to the release app version

--- androidHyperskillApp/src/release/java/org/hyperskill/app/android/gamification_toolbar/view/ui/delegate/RouterNavigateToProgressScreen.kt
+++
@@ -1,7 +0,0 @@
-package org.hyperskill.app.android.gamification_toolbar.view.ui.delegate
-
-import com.github.terrakok.cicerone.Router
-
-fun Router.navigateToProgressScreen() {
-    // TODO: replace with real navigation after approving ProgressScreen feature
-}
