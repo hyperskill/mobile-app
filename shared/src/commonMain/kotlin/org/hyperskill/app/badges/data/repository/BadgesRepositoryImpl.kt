765dad133679abf35d6cdf237a30e3dcbebbb00a
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2023-07-21 11:32:29 +0400

    Fix PR issues

--- shared/src/commonMain/kotlin/org/hyperskill/app/badges/data/repository/BadgesRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/badges/data/repository/BadgesRepositoryImpl.kt
@@ -1,11 +1,11 @@
 package org.hyperskill.app.badges.data.repository
 
+import org.hyperskill.app.badges.data.source.BadgesRemoteDataSource
 import org.hyperskill.app.badges.domain.model.Badge
 import org.hyperskill.app.badges.domain.repository.BadgesRepository
-import org.hyperskill.app.badges.remote.BadgesRemoteDataSourceImpl
 
 internal class BadgesRepositoryImpl(
-    private val remoteDataSource: BadgesRemoteDataSourceImpl
+    private val remoteDataSource: BadgesRemoteDataSource
 ) : BadgesRepository {
     override suspend fun getReceivedBadges(): Result<List<Badge>> =
         remoteDataSource.getReceivedBadges()
