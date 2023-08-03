1231cd924bfc333a8ea5b7ef0422cb600a45074c
vladkash <spanch-kash@yandex.ru>
2023-08-03 16:20:22 +0500

    enable loading badges from data source because production API is ready

--- shared/src/commonMain/kotlin/org/hyperskill/app/badges/injection/BadgesDataComponentImpl.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/badges/injection/BadgesDataComponentImpl.kt
@@ -5,7 +5,6 @@ import org.hyperskill.app.badges.data.source.BadgesRemoteDataSource
 import org.hyperskill.app.badges.domain.repository.BadgesRepository
 import org.hyperskill.app.badges.remote.BadgesRemoteDataSourceImpl
 import org.hyperskill.app.core.injection.AppGraph
-import org.hyperskill.app.debug.domain.model.EndpointConfigType
 
 internal class BadgesDataComponentImpl(
     appGraph: AppGraph
@@ -14,12 +13,6 @@ internal class BadgesDataComponentImpl(
     private val badgesRemoteDataSource: BadgesRemoteDataSource =
         BadgesRemoteDataSourceImpl(appGraph.networkComponent.authorizedHttpClient)
 
-    private val isBadgesFeatureEnabled: Boolean =
-        appGraph.commonComponent.buildKonfig.endpointConfigType != EndpointConfigType.PRODUCTION
-
     override val badgesRepository: BadgesRepository
-        get() = BadgesRepositoryImpl(
-            badgesRemoteDataSource,
-            isBadgesFeatureEnabled = isBadgesFeatureEnabled
-        )
+        get() = BadgesRepositoryImpl(badgesRemoteDataSource)
 }
