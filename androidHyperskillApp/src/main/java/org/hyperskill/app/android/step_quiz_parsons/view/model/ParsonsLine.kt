477b39f9f8c5fca287685064229d1bdfd9b3270c
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2023-08-31 19:33:45 +0400

    Parse line text as html

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/step_quiz_parsons/view/model/ParsonsLine.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/step_quiz_parsons/view/model/ParsonsLine.kt
@@ -1,10 +1,11 @@
 package org.hyperskill.app.android.step_quiz_parsons.view.model
 
+import android.text.Spanned
 import ru.nobird.app.core.model.Identifiable
 
 data class ParsonsLine(
     val lineNumber: Int,
-    val text: String,
+    val text: Spanned,
     val tabsCount: Int,
     val isSelected: Boolean
 ) : Identifiable<Int> {
