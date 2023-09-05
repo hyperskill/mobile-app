11272c112453e3b25c011e99fefcaa01841ce3fb
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2023-09-05 14:17:48 +0400

    Support fullScreenCodeEditor horizontal orientation

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/step_quiz_fullscreen_code/dialog/CodeStepQuizFullScreenDialogFragment.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/step_quiz_fullscreen_code/dialog/CodeStepQuizFullScreenDialogFragment.kt
@@ -1,6 +1,8 @@
 package org.hyperskill.app.android.step_quiz_fullscreen_code.dialog
 
 import android.app.Dialog
+import android.content.DialogInterface
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -26,6 +28,7 @@ import org.hyperskill.app.android.core.extensions.setTintList
 import org.hyperskill.app.android.databinding.DialogStepQuizCodeFullscreenBinding
 import org.hyperskill.app.android.latex.view.widget.LatexView
 import org.hyperskill.app.android.latex.view.widget.LatexWebView
+import org.hyperskill.app.android.main.view.ui.OrientationHost
 import org.hyperskill.app.android.step_quiz_code.view.delegate.CodeLayoutDelegate
 import org.hyperskill.app.android.step_quiz_code.view.delegate.CodeQuizInstructionDelegate
 import org.hyperskill.app.android.step_quiz_code.view.model.CodeStepQuizConfigFactory
@@ -116,6 +119,8 @@ class CodeStepQuizFullScreenDialogFragment : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        (requireActivity() as OrientationHost)
+            .requestOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
         setStyle(STYLE_NO_TITLE, R.style.ThemeOverlay_AppTheme_Dialog_Fullscreen)
         injectComponent()
     }
@@ -270,6 +275,11 @@ class CodeStepQuizFullScreenDialogFragment : DialogFragment() {
             syncCodeStateWithParent()
         }
         super.onPause()
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        (requireActivity() as OrientationHost).backToInitialOrientation()
     }
 
     fun onNewCode(code: String?) {
