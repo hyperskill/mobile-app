e8049f57954cce4b157304cdab347a0dece8e0a1
Ivan Magda <ivan.magda@hyperskill.org>
2023-09-07 13:04:34 +0400

    Add LottieAnimationViewWrapper

---
+++ iosHyperskillApp/iosHyperskillApp/Sources/Views/SwiftUI/Wrappers/LottieAnimationViewWrapper.swift
@@ -0,0 +1,50 @@
+import Lottie
+import SnapKit
+import SwiftUI
+
+struct LottieAnimationFileName {
+    let light: String
+    let dark: String
+}
+
+struct LottieAnimationViewWrapper: UIViewRepresentable {
+    let fileName: LottieAnimationFileName
+
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView(frame: .zero)
+
+        let fileNameForCurrentInterfaceStyle = view.isDarkInterfaceStyle ? fileName.dark : fileName.light
+
+        let animationView = LottieAnimationView()
+        animationView.animation = LottieAnimation.named(fileNameForCurrentInterfaceStyle)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .autoReverse
+        animationView.play()
+
+        view.addSubview(animationView)
+
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.snp.makeConstraints { make in
+            make.width.height.equalTo(view)
+        }
+
+        return view
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {}
+}
+
+struct LottieAnimationViewWrapper_Previews: PreviewProvider {
+    static var previews: some View {
+        LottieAnimationViewWrapper(
+            fileName: LottieAnimations.parsonsProblemOnboarding
+        )
+        .padding()
+
+        LottieAnimationViewWrapper(
+            fileName: LottieAnimations.parsonsProblemOnboarding
+        )
+        .padding()
+        .preferredColorScheme(.dark)
+    }
+}
