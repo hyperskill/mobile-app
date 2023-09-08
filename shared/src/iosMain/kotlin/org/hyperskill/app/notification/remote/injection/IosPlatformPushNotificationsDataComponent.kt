4baa6c3a70e0ed64a06f4b0e71da67d603ea1ccf
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2023-09-08 16:25:49 +0400

    Fix ktlint

--- shared/src/iosMain/kotlin/org/hyperskill/app/notification/remote/injection/IosPlatformPushNotificationsDataComponent.kt
+++ shared/src/iosMain/kotlin/org/hyperskill/app/notification/remote/injection/IosPlatformPushNotificationsDataComponent.kt
@@ -3,9 +3,7 @@ package org.hyperskill.app.notification.remote.injection
 import org.hyperskill.app.notification.remote.data.repository.IosFCMTokenRepository
 import org.hyperskill.app.notification.remote.domain.repository.FCMTokenRepository
 
-class IosPlatformPushNotificationsDataComponent(
-
-) : PlatformPushNotificationsDataComponent {
+class IosPlatformPushNotificationsDataComponent : PlatformPushNotificationsDataComponent {
     override val fcmTokenRepository: FCMTokenRepository
         get() = IosFCMTokenRepository()
 }
