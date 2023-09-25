1b630f4df0362851f566941133a6a2eb904064bf
Ivan Magda <ivan.magda@hyperskill.org>
2023-09-25 12:19:38 +0400

    Add UI tests target

---
+++ iosHyperskillApp/iosHyperskillAppUITests/LaunchPerformanceTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+
+final class LaunchPerformanceTests: XCTestCase {
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
