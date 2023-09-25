a1ce9b3fc0fef0dc480910142749120513f51440
Ivan Magda <ivan.magda@hyperskill.org>
2023-09-25 14:39:57 +0400

    Update StepQuizStatsTextMapper.getFormattedStepQuizStats

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuiz/ViewData/StepQuizViewDataMapper.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuiz/ViewData/StepQuizViewDataMapper.swift
@@ -31,7 +31,7 @@ final class StepQuizViewDataMapper {
         }
 
         let formattedStats = stepQuizStatsTextMapper.getFormattedStepQuizStats(
-            solvedByUsersCount: step.solvedBy,
+            solvedByCount: step.solvedBy,
             millisSinceLastCompleted: step.millisSinceLastCompleted
         )
 
