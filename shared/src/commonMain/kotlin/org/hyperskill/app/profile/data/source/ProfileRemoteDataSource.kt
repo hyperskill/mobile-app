c27171cc46ec4e0b18fcae8c21a828bf0522e146
Aleksandr Zhukov <alexandr.zhukov@hyperskill.org>
2023-09-29 10:26:53 +0400

    Shared send user time zone to api/profile (#683)
    ^ALTAPPS-960

--- shared/src/commonMain/kotlin/org/hyperskill/app/profile/data/source/ProfileRemoteDataSource.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/profile/data/source/ProfileRemoteDataSource.kt
@@ -1,5 +1,6 @@
 package org.hyperskill.app.profile.data.source
 
+import kotlinx.datetime.TimeZone
 import org.hyperskill.app.profile.domain.model.Profile
 
 interface ProfileRemoteDataSource {
@@ -8,4 +9,14 @@ interface ProfileRemoteDataSource {
     suspend fun selectTrackWithProject(profileId: Long, trackId: Long, projectId: Long): Result<Profile>
 
     suspend fun selectTrack(profileId: Long, trackId: Long): Result<Profile>
+
+    suspend fun setDailyStudyReminderNotificationHour(
+        profileId: Long,
+        notificationHour: Int,
+        timeZone: TimeZone
+    ): Result<Profile>
+
+    suspend fun setDailyStudyReminderNotificationHour(profileId: Long, notificationHour: Int?): Result<Profile>
+
+    suspend fun setTimeZone(profileId: Long, timeZone: TimeZone): Result<Profile>
 }
