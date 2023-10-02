ed7ebf6bbc3e08ae35ca37eaff5b5806885b3117
Ivan Magda <ivan.magda@hyperskill.org>
2023-10-02 16:10:42 +0400

    Introspect scroll view

---
+++ iosHyperskillApp/iosHyperskillAppTests/IntrospectTests/IntrospectScrollViewTests.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+import XCTest
+
+@testable import iosHyperskillApp
+
+final class IntrospectScrollViewTests: XCTestCase {
+    private struct ScrollViewTestView: View {
+        let spy: () -> Void
+
+        var body: some View {
+            NavigationView {
+                ScrollView {
+                    VStack {
+                        EmptyView()
+                    }
+                    .introspectScrollView { scrollView in
+                        self.spy()
+                    }
+                }
+            }
+        }
+    }
+
+    func testScrollView() {
+        let expectation = XCTestExpectation()
+        let view = ScrollViewTestView(spy: {
+            expectation.fulfill()
+        })
+        IntrospectTestUtils.present(view: view)
+        wait(for: [expectation], timeout: IntrospectTestUtils.Constants.timeout)
+    }
+}
