c0fe003210d6604ecef3da3bf91f2cb5ed349663
vladkash <spanch-kash@yandex.ru>
2023-10-02 17:09:12 +0500

    add feature with reducer for draft pull request

---
+++ shared/src/commonMain/kotlin/org/hyperskill/app/first_problem_onboarding/presentation/FirstProblemOnboardingFeature.kt
@@ -0,0 +1,67 @@
+package org.hyperskill.app.first_problem_onboarding.presentation
+
+import org.hyperskill.app.analytic.domain.model.hyperskill.HyperskillAnalyticEvent
+import org.hyperskill.app.learning_activities.domain.model.LearningActivity
+import org.hyperskill.app.profile.domain.model.Profile
+import org.hyperskill.app.step.domain.model.StepRoute
+
+object FirstProblemOnboardingFeature {
+    internal data class State(
+        val profileState: ProfileState,
+        val nextLearningActivityState: NextLearningActivityState,
+        val isNewUserMode: Boolean,
+        val isLearningActivityLoading: Boolean = false
+    )
+
+    internal sealed interface ProfileState {
+        object Idle : ProfileState
+        object Loading : ProfileState
+        object Error : ProfileState
+        data class Content(val profile: Profile) : ProfileState
+    }
+
+    internal sealed interface NextLearningActivityState {
+        object Idle : NextLearningActivityState
+        object Loading : NextLearningActivityState
+        object Error : NextLearningActivityState
+        data class Content(val nextLearningActivity: LearningActivity?) : NextLearningActivityState
+    }
+
+    sealed interface Message {
+        object Initialize : Message
+        object RetryContentLoading : Message
+        object LearningActionButtonClicked : Message
+
+        /**
+         * Analytic
+         * */
+        object ViewedEventMessage : Message
+    }
+
+    internal sealed interface FetchProfileResult : Message {
+        object Error : FetchProfileResult
+        data class Success(val profile: Profile) : FetchProfileResult
+    }
+
+    internal sealed interface FetchNextLearningActivityResult : Message {
+        object Error : FetchNextLearningActivityResult
+        data class Success(val nextLearningActivity: LearningActivity?) : FetchNextLearningActivityResult
+    }
+
+    sealed interface Action {
+        sealed interface ViewAction : Action {
+            object ShowNetworkError : ViewAction
+
+            sealed interface NavigateTo : ViewAction {
+                data class StepScreen(val stepRoute: StepRoute) : NavigateTo
+                object StudyPlanScreen : NavigateTo
+            }
+        }
+    }
+
+    internal sealed interface InternalAction : Action {
+        data class LogAnalyticsEvent(val event: HyperskillAnalyticEvent) : InternalAction
+        object FetchProfile : InternalAction
+        object FetchNextLearningActivity : InternalAction
+    }
+}
---
+++ shared/src/commonMain/kotlin/org/hyperskill/app/first_problem_onboarding/presentation/FirstProblemOnboardingReducer.kt
@@ -0,0 +1,119 @@
+package org.hyperskill.app.first_problem_onboarding.presentation
+
+import org.hyperskill.app.first_problem_onboarding.presentation.FirstProblemOnboardingFeature.Action
+import org.hyperskill.app.first_problem_onboarding.presentation.FirstProblemOnboardingFeature.InternalAction
+import org.hyperskill.app.first_problem_onboarding.presentation.FirstProblemOnboardingFeature.Message
+import org.hyperskill.app.first_problem_onboarding.presentation.FirstProblemOnboardingFeature.NextLearningActivityState
+import org.hyperskill.app.first_problem_onboarding.presentation.FirstProblemOnboardingFeature.ProfileState
+import org.hyperskill.app.first_problem_onboarding.presentation.FirstProblemOnboardingFeature.State
+import org.hyperskill.app.learning_activities.domain.model.LearningActivity
+import org.hyperskill.app.step.domain.model.StepRoute
+import ru.nobird.app.presentation.redux.reducer.StateReducer
+
+private typealias FirstProblemOnboardingReducerResult = Pair<State, Set<Action>>
+
+internal class FirstProblemOnboardingReducer : StateReducer<State, Message, Action> {
+    override fun reduce(state: State, message: Message): Pair<State, Set<Action>> =
+        when (message) {
+            Message.Initialize ->
+                handleInitialize(state)
+            Message.RetryContentLoading ->
+                handleRetryContentLoading(state)
+            is FirstProblemOnboardingFeature.FetchProfileResult ->
+                handleFetchProfileResult(state, message)
+            is FirstProblemOnboardingFeature.FetchNextLearningActivityResult ->
+                handleNextLearningActivityResult(state, message)
+            Message.LearningActionButtonClicked ->
+                handleLearningActionButtonClicked(state)
+            Message.ViewedEventMessage ->
+                TODO()
+        } ?: (state to emptySet())
+
+    private fun handleInitialize(state: State): FirstProblemOnboardingReducerResult? =
+        if (state.profileState is ProfileState.Idle &&
+            state.nextLearningActivityState is NextLearningActivityState.Idle
+        ) {
+            state.updateProfileState(ProfileState.Loading) to setOf(
+                InternalAction.FetchProfile,
+                InternalAction.FetchNextLearningActivity
+            )
+        } else {
+            null
+        }
+
+    private fun handleRetryContentLoading(state: State): FirstProblemOnboardingReducerResult? =
+        if (state.profileState is ProfileState.Error) {
+            state.updateProfileState(ProfileState.Loading) to setOf(InternalAction.FetchProfile)
+        } else {
+            null
+        }
+
+    private fun handleFetchProfileResult(
+        state: State,
+        message: FirstProblemOnboardingFeature.FetchProfileResult
+    ): FirstProblemOnboardingReducerResult? =
+        when (message) {
+            FirstProblemOnboardingFeature.FetchProfileResult.Error ->
+                state.updateProfileState(ProfileState.Error) to emptySet()
+            is FirstProblemOnboardingFeature.FetchProfileResult.Success ->
+                if (state.profileState is ProfileState.Loading) {
+                    state.updateProfileState(ProfileState.Content(message.profile)) to emptySet()
+                } else {
+                    null
+                }
+        }
+
+    private fun handleNextLearningActivityResult(
+        state: State,
+        message: FirstProblemOnboardingFeature.FetchNextLearningActivityResult
+    ): FirstProblemOnboardingReducerResult =
+        when (message) {
+            FirstProblemOnboardingFeature.FetchNextLearningActivityResult.Error ->
+                state
+                    .copy(isLearningActivityLoading = false)
+                    .updateNextLearningActivityState(NextLearningActivityState.Error) to buildSet {
+                    if (state.isLearningActivityLoading) {
+                        add(Action.ViewAction.ShowNetworkError)
+                    }
+                }
+            is FirstProblemOnboardingFeature.FetchNextLearningActivityResult.Success ->
+                if (state.isLearningActivityLoading) {
+                    state.copy(isLearningActivityLoading = false) to setOf(
+                        getNavigateActionByLearningActivity(message.nextLearningActivity)
+                    )
+                } else {
+                    state.updateNextLearningActivityState(
+                        NextLearningActivityState.Content(message.nextLearningActivity)
+                    ) to emptySet()
+                }
+        }
+
+    private fun handleLearningActionButtonClicked(state: State): FirstProblemOnboardingReducerResult? =
+        if (state.profileState is ProfileState.Content) {
+            when (state.nextLearningActivityState) {
+                NextLearningActivityState.Idle ->
+                    null
+                NextLearningActivityState.Error ->
+                    state.copy(isLearningActivityLoading = true) to setOf(InternalAction.FetchNextLearningActivity)
+                NextLearningActivityState.Loading ->
+                    state.copy(isLearningActivityLoading = true) to emptySet()
+                is NextLearningActivityState.Content ->
+                    state to setOf(
+                        getNavigateActionByLearningActivity(state.nextLearningActivityState.nextLearningActivity)
+                    )
+            }
+        } else {
+            null
+        }
+
+    private fun getNavigateActionByLearningActivity(learningActivity: LearningActivity?) =
+        learningActivity?.targetId?.let { stepId ->
+            Action.ViewAction.NavigateTo.StepScreen(StepRoute.Learn.Step(stepId))
+        } ?: Action.ViewAction.NavigateTo.StudyPlanScreen
+
+    private fun State.updateProfileState(profileState: ProfileState): State =
+        copy(profileState = profileState)
+
+    private fun State.updateNextLearningActivityState(learningActivityState: NextLearningActivityState): State =
+        copy(nextLearningActivityState = learningActivityState)
+}
