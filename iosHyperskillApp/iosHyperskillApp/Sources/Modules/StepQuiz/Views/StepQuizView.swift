523e86a4db1de27b5181899336e7e0177044b083
Ivan Magda <ivan.magda@hyperskill.org>
2023-11-01 12:51:01 +0400

    iOS changes

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuiz/Views/StepQuizView.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuiz/Views/StepQuizView.swift
@@ -68,12 +68,17 @@ struct StepQuizView: View {
                             onExpandButtonTap: viewModel.logClickedStepTextDetailsEvent
                         )
 
-                        if viewData.stepHasHints {
-                            StepQuizHintsAssembly(
-                                stepID: viewModel.step.id,
-                                stepRoute: viewModel.stepRoute
-                            ).makeModule()
-                        }
+                        StepQuizHintsView(
+                            state: viewModel.state.stepQuizHintsState,
+                            onNewMessage: { [weak viewModel] message in
+                                guard let viewModel else {
+                                    return
+                                }
+
+                                viewModel.onNewMessage(StepQuizFeatureMessageStepQuizHintsMessage(message: message))
+                            }
+                        )
+                        .equatable()
 
                         buildQuizContent(
                             state: viewModel.state,
@@ -305,6 +310,11 @@ struct StepQuizView: View {
             case .stepScreen(let navigateToStepScreenViewAction):
                 let assembly = StepAssembly(stepRoute: navigateToStepScreenViewAction.stepRoute)
                 stackRouter.pushViewController(assembly.makeModule())
+            }
+        case .stepQuizHintsViewAction(let stepQuizHintsViewAction):
+            switch StepQuizHintsFeatureActionViewActionKs(stepQuizHintsViewAction.viewAction) {
+            case .showNetworkError:
+                ProgressHUD.showError(status: Strings.Common.connectionError)
             }
         }
     }
---
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizHints/StepQuizHintsView.swift
@@ -0,0 +1,95 @@
+import shared
+import SVProgressHUD
+import SwiftUI
+
+extension StepQuizHintsView {
+    struct Appearance {
+        let skeletonInitialSize = CGSize(width: 146, height: 34)
+        let skeletonHintHeight: CGFloat = 152
+    }
+}
+
+struct StepQuizHintsView: View {
+    private(set) var appearance = Appearance()
+
+    var state: StepQuizHintsFeatureState
+    var onNewMessage: (StepQuizHintsFeatureMessage) -> Void
+
+    private var viewStateKs: StepQuizHintsFeatureViewStateKs {
+        let viewState = StepQuizHintsViewStateMapper.shared.mapState(state: state)
+        return StepQuizHintsFeatureViewStateKs(viewState)
+    }
+
+    var body: some View {
+        buildBody()
+    }
+
+    // MARK: Private API
+
+    @ViewBuilder
+    private func buildBody() -> some View {
+        switch viewStateKs {
+        case .idle, .initialLoading:
+            SkeletonRoundedView()
+                .frame(size: appearance.skeletonInitialSize)
+        case .hintLoading:
+            SkeletonRoundedView()
+                .frame(height: appearance.skeletonHintHeight)
+        case .error:
+            StepQuizShowHintButton(
+                text: Strings.Placeholder.networkErrorButtonText,
+                showLightning: false,
+                onClick: {
+                    onNewMessage(StepQuizHintsFeatureMessageLoadHintButtonClicked())
+                }
+            )
+        case .content(let sealedState):
+            buildContent(state: StepQuizHintsFeatureViewStateContentKs(sealedState))
+        }
+    }
+
+    @ViewBuilder
+    private func buildContent(state: StepQuizHintsFeatureViewStateContentKs) -> some View {
+        switch state {
+        case .seeHintButton:
+            StepQuizShowHintButton(
+                text: Strings.StepQuiz.Hints.showButton,
+                onClick: {
+                    onNewMessage(StepQuizHintsFeatureMessageLoadHintButtonClicked())
+                }
+            )
+        case .hintCard(let data):
+            StepQuizHintCardView(
+                authorAvatarSource: data.authorAvatar,
+                authorName: data.authorName,
+                hintText: data.hintText,
+                hintState: data.hintState,
+                onReactionTapped: { reaction in
+                    onNewMessage(StepQuizHintsFeatureMessageReactionButtonClicked(reaction: reaction))
+                },
+                onReportTapped: {
+                    onNewMessage(StepQuizHintsFeatureMessageClickedReportEventMessage())
+                },
+                onReportAlertAppeared: {
+                    onNewMessage(StepQuizHintsFeatureMessageReportHintNoticeShownEventMessage())
+                },
+                onReportAlertConfirmed: {
+                    onNewMessage(StepQuizHintsFeatureMessageReportHint())
+                    onNewMessage(StepQuizHintsFeatureMessageReportHintNoticeHiddenEventMessage(isReported: true))
+                },
+                onReportAlertCanceled: {
+                    onNewMessage(StepQuizHintsFeatureMessageReportHintNoticeHiddenEventMessage(isReported: true))
+                },
+                onNextHintTapped: {
+                    onNewMessage(StepQuizHintsFeatureMessageLoadHintButtonClicked())
+                }
+            )
+        }
+    }
+}
+
+extension StepQuizHintsView: Equatable {
+    static func == (lhs: StepQuizHintsView, rhs: StepQuizHintsView) -> Bool {
+        StepQuizHintsFeatureStateKs(lhs.state) == StepQuizHintsFeatureStateKs(rhs.state)
+    }
+}
