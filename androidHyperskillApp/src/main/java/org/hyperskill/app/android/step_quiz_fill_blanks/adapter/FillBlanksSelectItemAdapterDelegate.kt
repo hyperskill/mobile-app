960222d4d985bec1c2f5bd570fdfd8d17cb71b0a
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2023-11-10 12:48:58 +0400

    Extract models and adapterDelegates from FillBlanksStepQuizFormDelegate

---
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/step_quiz_fill_blanks/adapter/FillBlanksSelectItemAdapterDelegate.kt
@@ -0,0 +1,69 @@
+package org.hyperskill.app.android.step_quiz_fill_blanks.adapter
+
+import android.graphics.drawable.LayerDrawable
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.hyperskill.app.android.R
+import org.hyperskill.app.android.core.extensions.LayerListDrawableDelegate
+import org.hyperskill.app.android.step_quiz_fill_blanks.model.FillBlanksUiItem
+import org.hyperskill.app.step_quiz_fill_blanks.model.FillBlanksOption
+import ru.nobird.android.ui.adapterdelegates.AdapterDelegate
+import ru.nobird.android.ui.adapterdelegates.DelegateViewHolder
+import ru.nobird.android.view.base.ui.extension.setTextIfChanged
+
+class FillBlanksSelectItemAdapterDelegate(
+    private val options: List<FillBlanksOption>,
+    private val onClick: (blankIndex: Int, selectedOptionIndex: Int?) -> Unit
+) : AdapterDelegate<FillBlanksUiItem, DelegateViewHolder<FillBlanksUiItem>>() {
+    override fun isForViewType(position: Int, data: FillBlanksUiItem): Boolean =
+        data is FillBlanksUiItem.Select
+
+    override fun onCreateViewHolder(parent: ViewGroup): DelegateViewHolder<FillBlanksUiItem> =
+        ViewHolder(createView(parent, R.layout.item_step_quiz_fill_blanks_select))
+
+    inner class ViewHolder(root: View) : DelegateViewHolder<FillBlanksUiItem>(root) {
+        private val textView = itemView as TextView
+        private val layerListDrawableDelegate = LayerListDrawableDelegate(
+            listOf(
+                R.id.step_quiz_fill_blanks_select_empty_layer,
+                R.id.step_quiz_fill_blanks_select_filled_layer,
+                R.id.step_quiz_fill_blanks_select_empty_selected_layer,
+                R.id.step_quiz_fill_blanks_select_filled_selected_layer
+            ),
+            textView.background.mutate() as LayerDrawable
+        )
+
+        init {
+            textView.setOnClickListener {
+                val position = bindingAdapterPosition
+                val item = itemData as? FillBlanksUiItem.Select ?: return@setOnClickListener
+                val selectedOptionIndex = item.origin.selectedOptionIndex
+                if (position != RecyclerView.NO_POSITION) {
+                    onClick(position, selectedOptionIndex)
+                }
+            }
+        }
+
+        override fun onBind(data: FillBlanksUiItem) {
+            val selectItem = data as FillBlanksUiItem.Select
+            val selectedOptionIndex = selectItem.origin.selectedOptionIndex
+            val text = if (selectedOptionIndex != null) {
+                options.getOrNull(selectedOptionIndex)?.displayText
+            } else {
+                null
+            }
+            textView.isClickable = selectItem.isEnabled
+            textView.setTextIfChanged(text ?: "")
+            layerListDrawableDelegate.showLayer(
+                when {
+                    selectItem.isHighlighted && text.isNullOrEmpty() ->
+                        R.id.step_quiz_fill_blanks_select_empty_selected_layer
+                    text.isNullOrEmpty() -> R.id.step_quiz_fill_blanks_select_empty_layer
+                    else -> R.id.step_quiz_fill_blanks_select_filled_layer
+                }
+            )
+        }
+    }
+}
--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/step_quiz_fill_blanks/delegate/FillBlanksStepQuizFormDelegate.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/step_quiz_fill_blanks/delegate/FillBlanksStepQuizFormDelegate.kt
@@ -1,26 +1,24 @@
 package org.hyperskill.app.android.step_quiz_fill_blanks.delegate
 
-import android.graphics.drawable.LayerDrawable
-import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.core.text.HtmlCompat
-import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
-import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.FragmentManager
-import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.TransitionManager
 import com.google.android.flexbox.FlexboxItemDecoration
 import com.google.android.flexbox.FlexboxLayoutManager
 import org.hyperskill.app.android.R
-import org.hyperskill.app.android.core.extensions.LayerListDrawableDelegate
-import org.hyperskill.app.android.databinding.ItemStepQuizFillBlanksSelectOptionBinding
 import org.hyperskill.app.android.databinding.LayoutStepQuizFillBlanksBinding
 import org.hyperskill.app.android.databinding.LayoutStepQuizFillBlanksOptionsBinding
 import org.hyperskill.app.android.step_quiz.view.delegate.StepQuizFormDelegate
+import org.hyperskill.app.android.step_quiz_fill_blanks.adapter.FillBlanksInputItemAdapterDelegate
+import org.hyperskill.app.android.step_quiz_fill_blanks.adapter.FillBlanksSelectItemAdapterDelegate
+import org.hyperskill.app.android.step_quiz_fill_blanks.adapter.FillBlanksSelectOptionAdapterDelegate
+import org.hyperskill.app.android.step_quiz_fill_blanks.adapter.FillBlanksTextItemAdapterDelegate
 import org.hyperskill.app.android.step_quiz_fill_blanks.dialog.FillBlanksInputDialogFragment
 import org.hyperskill.app.android.step_quiz_fill_blanks.model.FillBlanksSelectOptionUIItem
 import org.hyperskill.app.android.step_quiz_fill_blanks.model.FillBlanksUiItem
+import org.hyperskill.app.android.step_quiz_fill_blanks.model.HighlightResult
+import org.hyperskill.app.android.step_quiz_fill_blanks.model.ResolveState
 import org.hyperskill.app.step_quiz.domain.model.submissions.Reply
 import org.hyperskill.app.step_quiz.presentation.StepQuizFeature
 import org.hyperskill.app.step_quiz.presentation.StepQuizResolver
@@ -31,9 +29,7 @@ import org.hyperskill.app.step_quiz_fill_blanks.model.FillBlanksOption
 import org.hyperskill.app.step_quiz_fill_blanks.model.InvalidFillBlanksConfigException
 import org.hyperskill.app.step_quiz_fill_blanks.presentation.FillBlanksItemMapper
 import org.hyperskill.app.step_quiz_fill_blanks.presentation.FillBlanksResolver
-import ru.nobird.android.ui.adapterdelegates.dsl.adapterDelegate
 import ru.nobird.android.ui.adapters.DefaultDelegateAdapter
-import ru.nobird.android.view.base.ui.extension.setTextIfChanged
 import ru.nobird.android.view.base.ui.extension.showIfNotExists
 import ru.nobird.app.core.model.mutate
 
@@ -45,10 +41,8 @@ class FillBlanksStepQuizFormDelegate(
 ) : StepQuizFormDelegate {
 
     private val fillBlanksAdapter = DefaultDelegateAdapter<FillBlanksUiItem>().apply {
-        addDelegate(textAdapterDelegate())
-        addDelegate(
-            inputAdapterDelegate(::onInputItemClick)
-        )
+        addDelegate(FillBlanksTextItemAdapterDelegate())
+        addDelegate(FillBlanksInputItemAdapterDelegate(::onInputItemClick))
     }
 
     private var fillBlanksOptionsAdapter: DefaultDelegateAdapter<FillBlanksSelectOptionUIItem>? = null
@@ -61,13 +55,6 @@ class FillBlanksStepQuizFormDelegate(
 
     private var selectItemIndices: List<Int> = emptyList()
     private var highlightedSelectItemIndex: Int? = null
-
-    private val selectOptionLayers = listOf(
-        R.id.step_quiz_fill_blanks_select_empty_layer,
-        R.id.step_quiz_fill_blanks_select_filled_layer,
-        R.id.step_quiz_fill_blanks_select_empty_selected_layer,
-        R.id.step_quiz_fill_blanks_select_filled_selected_layer
-    )
 
     init {
         with(binding.stepQuizFillBlanksRecycler) {
@@ -118,7 +105,7 @@ class FillBlanksStepQuizFormDelegate(
             fillBlanksData?.options?.let { options ->
                 // addDelegate adds delegate only once under the hood
                 fillBlanksAdapter.addDelegate(
-                    selectAdapterDelegate(options) { blankIndex, selectedOptionIndex ->
+                    FillBlanksSelectItemAdapterDelegate(options) { blankIndex, selectedOptionIndex ->
                         onSelectItemClick(
                             blankIndex = blankIndex,
                             selectedOptionIndex = selectedOptionIndex,
@@ -134,7 +121,6 @@ class FillBlanksStepQuizFormDelegate(
 
             fillBlanksAdapter.items = mapItems(fillBlanksData, highlightedSelectItemIndex, isEnabled)
             binding.root.post { binding.stepQuizFillBlanksRecycler.requestLayout() }
-
 
             fillBlanksOptionsAdapter?.items = mapBlankOptions(fillBlanksData, isEnabled)
         }
@@ -210,10 +196,10 @@ class FillBlanksStepQuizFormDelegate(
     private fun setupOptionsRecycler(optionsBinding: LayoutStepQuizFillBlanksOptionsBinding) {
         fillBlanksOptionsAdapter = DefaultDelegateAdapter<FillBlanksSelectOptionUIItem>().apply {
             addDelegate(
-                optionAdapterDelegate { selectedOptionIndex, option ->
+                FillBlanksSelectOptionAdapterDelegate { selectedOptionIndex, option ->
                     onOptionClick(
                         selectedOptionIndex = selectedOptionIndex,
-                        blankIndex = highlightedSelectItemIndex ?: return@optionAdapterDelegate,
+                        blankIndex = highlightedSelectItemIndex ?: return@FillBlanksSelectOptionAdapterDelegate,
                         selectedOption = option,
                         items = fillBlanksAdapter.items,
                         selectItemIndices = selectItemIndices,
@@ -281,116 +267,11 @@ class FillBlanksStepQuizFormDelegate(
         onQuizChanged(createReplyInternal(updatedItems))
     }
 
-    private fun textAdapterDelegate() =
-        adapterDelegate<FillBlanksUiItem, FillBlanksUiItem.Text>(R.layout.item_step_quiz_fill_blanks_text) {
-            val textView = itemView as TextView
-            onBind { textItem ->
-                textView.updateLayoutParams<FlexboxLayoutManager.LayoutParams> {
-                    isWrapBefore = textItem.origin.startsWithNewLine
-                }
-                textView.setTextIfChanged(
-                    HtmlCompat.fromHtml(textItem.origin.text, HtmlCompat.FROM_HTML_MODE_COMPACT)
-                )
-            }
-        }
-
-    private fun inputAdapterDelegate(onClick: (index: Int, String) -> Unit) =
-        adapterDelegate<FillBlanksUiItem, FillBlanksUiItem.Input>(R.layout.item_step_quiz_fill_blanks_input) {
-            val textView = itemView as TextView
-            textView.setOnClickListener {
-                val position = bindingAdapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    onClick(position, textView.text.toString())
-                }
-            }
-            onBind { inputItem ->
-                textView.isClickable = inputItem.isEnabled
-                textView.setTextIfChanged(inputItem.origin.inputText ?: "")
-            }
-        }
-
-    private fun onInputItemClick(index: Int, text: String) {
+    private fun onInputItemClick(inputItemIndex: Int, text: String) {
         FillBlanksInputDialogFragment
-            .newInstance(index, text)
+            .newInstance(inputItemIndex, text)
             .showIfNotExists(fragmentManager, FillBlanksInputDialogFragment.TAG)
     }
-
-    private fun selectAdapterDelegate(
-        options: List<FillBlanksOption>,
-        onClick: (blankIndex: Int, selectedOptionIndex: Int?) -> Unit
-    ) =
-        adapterDelegate<FillBlanksUiItem, FillBlanksUiItem.Select>(R.layout.item_step_quiz_fill_blanks_select) {
-            val textView = itemView as TextView
-            val layerListDrawableDelegate = LayerListDrawableDelegate(
-                selectOptionLayers,
-                textView.background.mutate() as LayerDrawable
-            )
-
-            textView.setOnClickListener {
-                val position = bindingAdapterPosition
-                val selectedOptionIndex = item?.origin?.selectedOptionIndex
-                if (position != RecyclerView.NO_POSITION) {
-                    onClick(position, selectedOptionIndex)
-                }
-            }
-
-            onBind { selectItem ->
-                val selectedOptionIndex = selectItem.origin.selectedOptionIndex
-                val text = if (selectedOptionIndex != null) {
-                    options.getOrNull(selectedOptionIndex)?.displayText
-                } else {
-                    null
-                }
-                textView.isClickable = selectItem.isEnabled
-                textView.setTextIfChanged(text ?: "")
-                layerListDrawableDelegate.showLayer(
-                    when {
-                        selectItem.isHighlighted && text.isNullOrEmpty() ->
-                            R.id.step_quiz_fill_blanks_select_empty_selected_layer
-                        text.isNullOrEmpty() -> R.id.step_quiz_fill_blanks_select_empty_layer
-                        else -> R.id.step_quiz_fill_blanks_select_filled_layer
-                    }
-                )
-            }
-        }
-
-    private fun optionAdapterDelegate(
-        onClick: (selectedOptionIndex: Int, option: FillBlanksOption) -> Unit
-    ) =
-        adapterDelegate<FillBlanksSelectOptionUIItem, FillBlanksSelectOptionUIItem>(
-            R.layout.item_step_quiz_fill_blanks_select_option
-        ) {
-            val binding = ItemStepQuizFillBlanksSelectOptionBinding.bind(itemView)
-
-            binding.stepQuizFillBlanksSelectOptionContainer.setOnClickListener {
-                val position = bindingAdapterPosition
-                val item = item
-                if (position != RecyclerView.NO_POSITION && item != null) {
-                    onClick(position, item.option)
-                }
-            }
-
-            val layerListDrawableDelegate = LayerListDrawableDelegate(
-                selectOptionLayers,
-                binding.stepQuizFillBlanksSelectOptionContainer.background.mutate() as LayerDrawable
-            )
-
-            onBind { selectOption ->
-                with(binding.stepQuizFillBlanksSelectOptionText) {
-                    isInvisible = selectOption.isUsed
-                    setTextIfChanged(selectOption.option.displayText)
-                }
-                binding.stepQuizFillBlanksSelectOptionContainer.isClickable =
-                    selectOption.isClickable && !selectOption.isUsed
-                layerListDrawableDelegate.showLayer(
-                    if (selectOption.isUsed) {
-                        R.id.step_quiz_fill_blanks_select_empty_layer
-                    } else {
-                        R.id.step_quiz_fill_blanks_select_filled_layer
-                    }
-                )
-            }
-        }
 
     private fun onSelectItemClick(
         blankIndex: Int,
@@ -518,15 +399,4 @@ class FillBlanksStepQuizFormDelegate(
                 )
             }
         }
-
-    private sealed class ResolveState {
-        object NotResolved : ResolveState()
-        data class ResolveSucceed(val mode: FillBlanksMode) : ResolveState()
-        object ResolveFailed : ResolveState()
-    }
-
-    private class HighlightResult(
-        val updatedItems: List<FillBlanksUiItem>,
-        val newHighlightedSelectItemIndex: Int?
-    )
 }
