1a9d86118bdcc6fd2ec32f788c92fc1413de16fd
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2023-11-20 18:26:33 +0400

    Add missing card border

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/challenge/ui/ChallengeScaffold.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/challenge/ui/ChallengeScaffold.kt
@@ -1,11 +1,16 @@
 package org.hyperskill.app.android.challenge.ui
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
+import org.hyperskill.app.R
 import org.hyperskill.app.android.core.view.ui.widget.compose.HyperskillCard
 
 @Composable
@@ -17,7 +22,13 @@ fun ChallengeScaffold(
     HyperskillCard(
         contentPadding = ChallengeCardDefaults.paddingValues,
         onClick = onClick,
-        modifier = modifier
+        modifier = modifier.border(
+            width = 1.dp,
+            color = colorResource(id = R.color.color_on_surface_alpha_9),
+            shape = RoundedCornerShape(
+                dimensionResource(id = org.hyperskill.app.android.R.dimen.corner_radius)
+            )
+        )
     ) {
         Column(
             content = content,
