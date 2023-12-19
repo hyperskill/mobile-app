fe94ea125d5af8b84dae9828c3513fddf1a5f434
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2023-12-19 16:15:50 +0100

    Remove redundant error on iOS side

--- iosHyperskillApp/iosHyperskillApp/Sources/Extensions/ThirdParty/MokoResources/ResourcesColorResourceThemed+UIColor.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Extensions/ThirdParty/MokoResources/ResourcesColorResourceThemed+UIColor.swift
@@ -4,8 +4,7 @@ import UIKit
 extension ColorDesc {
     var uiColor: UIColor {
         switch self {
-        case let descResource as ColorDescResource:
-            return descResource.resource.getUIColor()
+        case let descResource as ColorDescResource: return descResource.resource.getUIColor()
         case let singleColor as ColorDescSingle:
             return singleColor.color.uiColor
         case let themedColor as ColorDescThemed:
@@ -19,17 +18,5 @@ extension ColorDesc {
 extension ColorDescThemed {
     var dynamicUIColor: UIColor {
         UIColor.dynamic(light: self.lightColor.uiColor, dark: self.darkColor.uiColor)
-    }
-}
-
-private struct ColorHandlingError : LocalizedError {
-    let description: String
-
-    init(_ description: String) {
-        self.description = description
-    }
-
-    var errorDescription: String? {
-        description
     }
 }
