e780b638869c9567811bb6d8fd816695a27732e1
Ivan Magda <ivan.magda@hyperskill.org>
2024-01-08 15:07:21 +0700

    Use streak freeze type query param

--- shared/src/commonMain/kotlin/org/hyperskill/app/products/remote/model/GetProductsRequest.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/products/remote/model/GetProductsRequest.kt
@@ -1,19 +1,18 @@
 package org.hyperskill.app.products.remote.model
 
 class GetProductsRequest(
-    val category: Category
+    val type: ProductType
 ) {
     companion object {
-        private const val PARAM_CATEGORY = "category"
+        private const val PARAM_TYPE = "type"
     }
 
     val parameters: Map<String, Any> =
         mapOf(
-            PARAM_CATEGORY to category.parameterValue
+            PARAM_TYPE to type.parameterValue
         )
 
-    enum class Category(val parameterValue: String) {
-        STREAK("Streak"),
-        COVER("Cover")
+    enum class ProductType(val parameterValue: String) {
+        STREAK_FREEZE("streak freeze")
     }
 }
