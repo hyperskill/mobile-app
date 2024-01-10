f48cbedc4744221df9d1396e44147a9eb6c274c9
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2024-01-10 19:51:01 +0100

    Rename android app

--- shared/src/commonMain/kotlin/org/hyperskill/app/core/domain/platform/Platform.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/core/domain/platform/Platform.kt
@@ -1,5 +1,7 @@
 package org.hyperskill.app.core.domain.platform
 
+import dev.icerock.moko.resources.StringResource
+
 expect class Platform() {
     val platformType: PlatformType
     val platformDescription: String
@@ -7,4 +9,6 @@ expect class Platform() {
     val analyticName: String
 
     val feedbackName: String
+
+    val appNameResource: StringResource
 }
