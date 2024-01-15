0c95dc2a8dcbd39c1bc0774339d584faa5da4ba0
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2024-01-15 12:00:33 +0100

    Remove a redundant isMobileOnly extension

--- shared/src/commonMain/kotlin/org/hyperskill/app/subscriptions/domain/model/Subscription.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/subscriptions/domain/model/Subscription.kt
@@ -21,6 +21,3 @@ data class Subscription(
 
 val Subscription.isFreemium: Boolean
     get() = type == SubscriptionType.FREEMIUM
-
-val Subscription.isMobileOnly: Boolean
-    get() = type == SubscriptionType.MOBILE_ONLY
