53dc23b9ce5a13a053e40bb94bda1858e9d55efe
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2024-01-16 18:06:53 +0100

    Add revenue cat key

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.android.build.api.dsl.LibraryBuildType
 import com.codingfeline.buildkonfig.compiler.FieldSpec.Type.BOOLEAN
 import com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING
 import java.time.Year
@@ -7,6 +8,7 @@ import org.jetbrains.dokka.base.DokkaBaseConfiguration
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.plugin.mpp.Framework
 import org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink
+import org.jetbrains.kotlin.konan.properties.Properties
 import org.jetbrains.kotlin.konan.properties.loadProperties
 import org.jetbrains.kotlin.konan.properties.propertyString
 
@@ -153,6 +155,26 @@ android {
 
     sourceSets {
         getByName("main").java.srcDirs("build/generated/moko/androidMain/src")
+    }
+
+    buildTypes {
+        fun applyFlavorConfigsFromFile(libraryBuildType: LibraryBuildType) {
+            if (SystemProperties.isCI() && !SystemProperties.isGitCryptUnlocked()) return
+            val properties: Properties =
+                loadProperties("${project.rootDir}/shared/src/androidMain/keys/revenuecat.properties")
+            properties.keys.forEach { name ->
+                name as String
+                libraryBuildType.buildConfigField(
+                    type = "String",
+                    name = name,
+                    value = requireNotNull(properties.propertyString(name))
+                )
+            }
+        }
+        
+        all {
+            applyFlavorConfigsFromFile(this)
+        }
     }
 }
 
--- shared/src/androidMain/kotlin/org/hyperskill/app/purchases/domain/AndroidPurchaseManager.kt
+++ shared/src/androidMain/kotlin/org/hyperskill/app/purchases/domain/AndroidPurchaseManager.kt
@@ -3,7 +3,7 @@ package org.hyperskill.app.purchases.domain
 import android.content.Context
 import com.revenuecat.purchases.Purchases
 import com.revenuecat.purchases.PurchasesConfiguration
-import org.hyperskill.app.R
+import org.hyperskill.app.BuildConfig
 import org.hyperskill.app.purchases.domain.model.PurchaseManager
 
 class AndroidPurchaseManager(
@@ -13,7 +13,7 @@ class AndroidPurchaseManager(
         Purchases.configure(
             PurchasesConfiguration.Builder(
                 context = context,
-                apiKey = context.getString(R.string.revenue_cat_api_key)
+                apiKey = BuildConfig.REVENUE_CAT_GOOGLE_API_KEY
             ).build()
         )
     }
