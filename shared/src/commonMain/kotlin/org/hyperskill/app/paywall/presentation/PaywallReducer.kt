1931d3aebe5af186dd85cc22a54cdc48bcd17fc2
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2024-01-22 14:19:16 +0100

    Add viewed analytic event

--- shared/src/commonMain/kotlin/org/hyperskill/app/paywall/presentation/PaywallReducer.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/paywall/presentation/PaywallReducer.kt
@@ -1,6 +1,8 @@
 package org.hyperskill.app.paywall.presentation
 
+import org.hyperskill.app.paywall.domain.analytic.PaywallViewedHyperskillAnalyticEvent
 import org.hyperskill.app.paywall.presentation.PaywallFeature.Action
+import org.hyperskill.app.paywall.presentation.PaywallFeature.InternalAction
 import org.hyperskill.app.paywall.presentation.PaywallFeature.Message
 import org.hyperskill.app.paywall.presentation.PaywallFeature.State
 import ru.nobird.app.presentation.redux.reducer.StateReducer
@@ -10,6 +12,11 @@ private typealias PaywallReducerResult = Pair<State, Set<Action>>
 class PaywallReducer : StateReducer<State, Message, Action> {
     override fun reduce(state: State, message: Message): PaywallReducerResult =
         when (message) {
-            Message.ViewedEventMessage -> TODO()
+            Message.ViewedEventMessage ->
+                state to setOf(
+                    InternalAction.LogAnalyticsEvent(
+                        PaywallViewedHyperskillAnalyticEvent
+                    )
+                )
         }
 }
