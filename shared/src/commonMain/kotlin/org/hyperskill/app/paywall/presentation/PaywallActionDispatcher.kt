28fdc9ac62da0bc97bfc77700b7a4eec61ae1737
Aleksandr Zhukov <alexandr.zhukov@hyperskill.org>
2024-02-07 17:35:15 +0400

    Shared paywall subscription purchase (#876)

--- shared/src/commonMain/kotlin/org/hyperskill/app/paywall/presentation/PaywallActionDispatcher.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/paywall/presentation/PaywallActionDispatcher.kt
@@ -4,23 +4,37 @@ import co.touchlab.kermit.Logger
 import org.hyperskill.app.analytic.domain.interactor.AnalyticInteractor
 import org.hyperskill.app.core.presentation.ActionDispatcherOptions
 import org.hyperskill.app.paywall.presentation.PaywallFeature.Action
+import org.hyperskill.app.paywall.presentation.PaywallFeature.InternalAction
 import org.hyperskill.app.paywall.presentation.PaywallFeature.InternalMessage
 import org.hyperskill.app.paywall.presentation.PaywallFeature.Message
 import org.hyperskill.app.purchases.domain.interactor.PurchaseInteractor
+import org.hyperskill.app.purchases.domain.model.PurchaseResult
+import org.hyperskill.app.sentry.domain.interactor.SentryInteractor
+import org.hyperskill.app.sentry.domain.model.transaction.HyperskillSentryTransactionBuilder
+import org.hyperskill.app.sentry.domain.withTransaction
+import org.hyperskill.app.subscriptions.domain.repository.SubscriptionsRepository
 import ru.nobird.app.presentation.redux.dispatcher.CoroutineActionDispatcher
 
 class PaywallActionDispatcher(
     config: ActionDispatcherOptions,
     private val analyticInteractor: AnalyticInteractor,
     private val purchaseInteractor: PurchaseInteractor,
+    private val subscriptionsRepository: SubscriptionsRepository,
+    private val sentryInteractor: SentryInteractor,
     private val logger: Logger
 ) : CoroutineActionDispatcher<Action, Message>(config.createConfig()) {
     override suspend fun doSuspendableAction(action: Action) {
         when (action) {
-            is PaywallFeature.InternalAction.LogAnalyticsEvent ->
+            is InternalAction.LogAnalyticsEvent ->
                 analyticInteractor.logEvent(action.event)
-            is PaywallFeature.InternalAction.FetchMobileOnlyPrice ->
+            is InternalAction.FetchMobileOnlyPrice ->
                 handleFetchMobileOnlyPrice(::onNewMessage)
+            is InternalAction.StartMobileOnlySubscriptionPurchase ->
+                handleStartMobileOnlySubscriptionPurchase(action, ::onNewMessage)
+            is InternalAction.SyncSubscription ->
+                handleSyncSubscription(::onNewMessage)
+            is InternalAction.LogWrongSubscriptionTypeAfterSync ->
+                handleLogWrongSubscriptionTypeAfterSync(action)
             else -> {
                 // no op
             }
@@ -30,25 +44,72 @@ class PaywallActionDispatcher(
     private suspend fun handleFetchMobileOnlyPrice(
         onNewMessage: (Message) -> Unit
     ) {
-        purchaseInteractor.getFormattedMobileOnlySubscriptionPrice()
-            .fold(
-                onSuccess = { price ->
-                    if (price != null) {
-                        InternalMessage.FetchMobileOnlyPriceSuccess(price)
-                    } else {
-                        logger.e {
-                            "Receive null instead of formatter mobile-only subscription price"
-                        }
-                        InternalMessage.FetchMobileOnlyPriceError
-                    }
-                },
-                onFailure = {
-                    logger.e(it) {
-                        "Error during mobile-only subscription price fetching"
-                    }
-                    InternalMessage.FetchMobileOnlyPriceError
-                }
-            )
-            .let(onNewMessage)
+        sentryInteractor.withTransaction(
+            transaction = HyperskillSentryTransactionBuilder.buildPaywallFetchSubscriptionPrice(),
+            onError = {
+                InternalMessage.FetchMobileOnlyPriceError
+            }
+        ) {
+            val price = purchaseInteractor
+                .getFormattedMobileOnlySubscriptionPrice()
+                .getOrThrow()
+            if (price != null) {
+                InternalMessage.FetchMobileOnlyPriceSuccess(price)
+            } else {
+                logger.e { "Receive null instead of formatted mobile-only subscription price" }
+                InternalMessage.FetchMobileOnlyPriceError
+            }
+        }.let(onNewMessage)
+    }
+
+    private suspend fun handleStartMobileOnlySubscriptionPurchase(
+        action: InternalAction.StartMobileOnlySubscriptionPurchase,
+        onNewMessage: (Message) -> Unit
+    ) {
+        sentryInteractor.withTransaction(
+            transaction = HyperskillSentryTransactionBuilder.buildPaywallFeaturePurchaseSubscription(),
+            onError = {
+                InternalMessage.MobileOnlySubscriptionPurchaseError
+            }
+        ) {
+            val purchaseResult = purchaseInteractor
+                .purchaseMobileOnlySubscription(action.purchaseParams)
+                .getOrThrow()
+            if (purchaseResult is PurchaseResult.Error) {
+                logger.e { getPurchaseErrorMessage(purchaseResult) }
+            }
+            InternalMessage.MobileOnlySubscriptionPurchaseSuccess(purchaseResult)
+        }.let(onNewMessage)
+    }
+
+    private fun getPurchaseErrorMessage(error: PurchaseResult.Error): String =
+        "Subscription purchase failed!\n${error.message}\n${error.underlyingErrorMessage}"
+
+    private suspend fun handleSyncSubscription(
+        onNewMessage: (Message) -> Unit
+    ) {
+        sentryInteractor.withTransaction(
+            transaction = HyperskillSentryTransactionBuilder.buildPaywallFeatureSyncSubscription(),
+            onError = {
+                InternalMessage.SubscriptionSyncError
+            }
+        ) {
+            subscriptionsRepository
+                .syncSubscription()
+                .getOrThrow()
+                .let(InternalMessage::SubscriptionSyncSuccess)
+        }.let(onNewMessage)
+    }
+
+    private fun handleLogWrongSubscriptionTypeAfterSync(
+        action: InternalAction.LogWrongSubscriptionTypeAfterSync
+    ) {
+        logger.e {
+            """
+                Wrong subscription type after sync:
+                expected=${action.expectedSubscriptionType}
+                actual=${action.actualSubscriptionType}
+            """.trimIndent()
+        }
     }
 }
--- shared/src/commonMain/kotlin/org/hyperskill/app/paywall/presentation/PaywallReducer.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/paywall/presentation/PaywallReducer.kt
@@ -7,13 +7,16 @@ import org.hyperskill.app.paywall.domain.analytic.PaywallViewedHyperskillAnalyti
 import org.hyperskill.app.paywall.domain.model.PaywallTransitionSource
 import org.hyperskill.app.paywall.presentation.PaywallFeature.Action
 import org.hyperskill.app.paywall.presentation.PaywallFeature.InternalAction
+import org.hyperskill.app.paywall.presentation.PaywallFeature.InternalMessage
 import org.hyperskill.app.paywall.presentation.PaywallFeature.Message
 import org.hyperskill.app.paywall.presentation.PaywallFeature.State
+import org.hyperskill.app.purchases.domain.model.PurchaseResult
+import org.hyperskill.app.subscriptions.domain.model.SubscriptionType
 import ru.nobird.app.presentation.redux.reducer.StateReducer
 
 private typealias ReducerResult = Pair<State, Set<Action>>
 
-class PaywallReducer(
+internal class PaywallReducer(
     private val paywallTransitionSource: PaywallTransitionSource
 ) : StateReducer<State, Message, Action> {
     override fun reduce(state: State, message: Message): ReducerResult =
@@ -30,12 +33,20 @@ class PaywallReducer(
                     )
                 )
             Message.ViewedEventMessage -> handleViewedEventMessage(state)
-            Message.BuySubscriptionClicked -> handleBuySubscriptionClicked(state)
+            is Message.BuySubscriptionClicked -> handleBuySubscriptionClicked(state, message)
             Message.ContinueWithLimitsClicked -> handleContinueWithLimitsClicked(state)
-            is PaywallFeature.InternalMessage.FetchMobileOnlyPriceSuccess ->
+            is InternalMessage.FetchMobileOnlyPriceSuccess ->
                 handleFetchMobileOnlyPriceSuccess(message)
-            PaywallFeature.InternalMessage.FetchMobileOnlyPriceError ->
+            InternalMessage.FetchMobileOnlyPriceError ->
                 handleFetchMobileOnlyPriceError()
+            is InternalMessage.MobileOnlySubscriptionPurchaseSuccess ->
+                handleMobileOnlySubscriptionPurchaseSuccess(state, message)
+            InternalMessage.MobileOnlySubscriptionPurchaseError ->
+                handlePurchaseError(state)
+            is InternalMessage.SubscriptionSyncSuccess ->
+                handleSubscriptionSyncSuccess(state, message)
+            InternalMessage.SubscriptionSyncError ->
+                handleSubscriptionSyncError(state)
         }
 
     private fun fetchMobileOnlyPrice(
@@ -56,14 +67,16 @@ class PaywallReducer(
         )
 
     private fun handleBuySubscriptionClicked(
-        state: State
+        state: State,
+        message: Message.BuySubscriptionClicked
     ): ReducerResult =
         state to setOf(
             InternalAction.LogAnalyticsEvent(
                 PaywallClickedBuySubscriptionHyperskillAnalyticEvent(
                     paywallTransitionSource
                 )
-            )
+            ),
+            InternalAction.StartMobileOnlySubscriptionPurchase(message.purchaseParams)
         )
 
     private fun handleContinueWithLimitsClicked(
@@ -75,14 +88,103 @@ class PaywallReducer(
                     paywallTransitionSource
                 )
             ),
-            Action.ViewAction.CompletePaywall
+            getTargetScreenNavigationAction(paywallTransitionSource)
         )
 
     private fun handleFetchMobileOnlyPriceSuccess(
-        message: PaywallFeature.InternalMessage.FetchMobileOnlyPriceSuccess
+        message: InternalMessage.FetchMobileOnlyPriceSuccess
     ): ReducerResult =
         State.Content(message.formattedPrice) to emptySet()
 
     private fun handleFetchMobileOnlyPriceError(): ReducerResult =
         State.Error to setOf()
+
+    private fun handleMobileOnlySubscriptionPurchaseSuccess(
+        state: State,
+        message: InternalMessage.MobileOnlySubscriptionPurchaseSuccess
+    ): ReducerResult =
+        if (state is State.Content) {
+            when (message.purchaseResult) {
+                is PurchaseResult.Succeed,
+                is PurchaseResult.Error.ProductAlreadyPurchasedError -> {
+                    state.copy(isPurchaseSyncLoadingShowed = true) to
+                        setOf(InternalAction.SyncSubscription)
+                }
+                PurchaseResult.CancelledByUser -> state to emptySet()
+                is PurchaseResult.Error.PaymentPendingError -> {
+                    state to setOf(
+                        Action.ViewAction.ShowMessage(
+                            PaywallFeature.MessageKind.PENDING_PURCHASE
+                        ),
+                        getTargetScreenNavigationAction(paywallTransitionSource)
+                    )
+                }
+
+                is PurchaseResult.Error.ErrorWhileFetchingProduct,
+                is PurchaseResult.Error.NoProductFound,
+                is PurchaseResult.Error.PurchaseNotAllowedError,
+                is PurchaseResult.Error.ReceiptAlreadyInUseError,
+                is PurchaseResult.Error.StoreProblemError,
+                is PurchaseResult.Error.OtherError -> handlePurchaseError(state)
+            }
+        } else {
+            state to emptySet()
+        }
+
+    private fun handlePurchaseError(
+        state: State
+    ): ReducerResult =
+        state to setOf(
+            Action.ViewAction.ShowMessage(
+                PaywallFeature.MessageKind.GENERAL
+            )
+        )
+
+    private fun handleSubscriptionSyncSuccess(
+        state: State,
+        message: InternalMessage.SubscriptionSyncSuccess
+    ): ReducerResult =
+        if (state is State.Content) {
+            state.copy(isPurchaseSyncLoadingShowed = false) to
+                if (message.subscription.type == SubscriptionType.MOBILE_ONLY) {
+                    setOf(getTargetScreenNavigationAction(paywallTransitionSource))
+                } else {
+                    setOf(
+                        Action.ViewAction.ShowMessage(
+                            PaywallFeature.MessageKind.SUBSCRIPTION_WILL_BECOME_AVAILABLE_SOON
+                        ),
+                        InternalAction.LogWrongSubscriptionTypeAfterSync(
+                            expectedSubscriptionType = SubscriptionType.MOBILE_ONLY,
+                            actualSubscriptionType = message.subscription.type
+                        )
+                    )
+                }
+        } else {
+            state to emptySet()
+        }
+
+    private fun handleSubscriptionSyncError(
+        state: State
+    ): ReducerResult =
+        if (state is State.Content) {
+            state.copy(isPurchaseSyncLoadingShowed = false) to setOf(
+                Action.ViewAction.ShowMessage(
+                    PaywallFeature.MessageKind.SUBSCRIPTION_WILL_BECOME_AVAILABLE_SOON
+                ),
+                getTargetScreenNavigationAction(paywallTransitionSource)
+            )
+        } else {
+            state to emptySet()
+        }
+
+    private fun getTargetScreenNavigationAction(
+        paywallTransitionSource: PaywallTransitionSource
+    ): Action.ViewAction =
+        when (paywallTransitionSource) {
+            PaywallTransitionSource.LOGIN ->
+                Action.ViewAction.CompletePaywall
+            PaywallTransitionSource.PROFILE_SETTINGS ->
+                // TODO: replace with navigation the Subscription management
+                Action.ViewAction.CompletePaywall
+        }
 }
