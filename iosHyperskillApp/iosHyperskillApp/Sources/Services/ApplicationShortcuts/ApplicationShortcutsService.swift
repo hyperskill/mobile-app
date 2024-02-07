2d8944a73d1646206c21d4e949eaeddc7ca0e721
Ivan Magda <ivan.magda@hyperskill.org>
2024-02-07 14:01:09 +0800

    Handle quick actions

---
+++ iosHyperskillApp/iosHyperskillApp/Sources/Services/ApplicationShortcuts/ApplicationShortcutsService.swift
@@ -0,0 +1,82 @@
+import shared
+import UIKit
+
+protocol ApplicationShortcutsServiceProtocol: AnyObject {
+    func handleShortcutItem(_ shortcutItem: UIApplicationShortcutItem) -> Bool
+    func handleLaunchOptions(_ launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+}
+
+final class ApplicationShortcutsService: ApplicationShortcutsServiceProtocol {
+    private lazy var applicationShortcutsInteractor: ApplicationShortcutsInteractor =
+        AppGraphBridge.sharedAppGraph.buildApplicationShortcutsDataComponent().applicationShortcutsInteractor
+
+    // MARK: Protocol Conforming
+
+    func handleLaunchOptions(_ launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if let shortcutItem = launchOptions?[.shortcutItem] as? UIApplicationShortcutItem {
+            _ = handleShortcutItem(shortcutItem)
+            return true
+        }
+        return false
+    }
+
+    func handleShortcutItem(_ shortcutItem: UIApplicationShortcutItem) -> Bool {
+        let shortcutType = shortcutItem.type
+        // TODO: Add analytics
+
+        guard let shortcutIdentifier = ApplicationShortcutIdentifier(fullIdentifier: shortcutType) else {
+            #if DEBUG
+            print("ApplicationShortcutsService: Did receive unknown shortcut identifier: \(shortcutType)")
+            #endif
+            return false
+        }
+
+        DispatchQueue.main.async {
+            self.performAction(for: shortcutIdentifier)
+        }
+
+        return true
+    }
+
+    // MARK: Private API
+
+    private func performAction(for shortcutIdentifier: ApplicationShortcutIdentifier) {
+        switch shortcutIdentifier {
+        case .sendFeedback:
+            performSendFeedback()
+        }
+    }
+
+    private func performSendFeedback() {
+        applicationShortcutsInteractor.getSendFeedbackEmailData { feedbackEmailData, error in
+            if let error = error {
+                #if DEBUG
+                print("ApplicationShortcutsService: SendFeedback, failed get email data: \(error)")
+                #endif
+                return
+            }
+
+            guard let feedbackEmailData = feedbackEmailData else {
+                #if DEBUG
+                print("ApplicationShortcutsService: SendFeedback, no email data")
+                #endif
+                return
+            }
+
+            assert(Thread.current.isMainThread)
+
+            guard let currentPresentedViewController = SourcelessRouter().currentPresentedViewController() else {
+                #if DEBUG
+                print("ApplicationShortcutsService: SendFeedback, no current presented view controller")
+                #endif
+                return
+            }
+
+            let sendEmailFeedbackController = SendEmailFeedbackController()
+            sendEmailFeedbackController.sendFeedback(
+                feedbackEmailData: feedbackEmailData,
+                presentationController: currentPresentedViewController
+            )
+        }
+    }
+}
--- shared/src/iosMain/kotlin/org/hyperskill/app/core/injection/IosAppComponent.kt
+++ shared/src/iosMain/kotlin/org/hyperskill/app/core/injection/IosAppComponent.kt
@@ -1,7 +1,10 @@
 package org.hyperskill.app.core.injection
 
+import org.hyperskill.app.application_shortcuts.injection.ApplicationShortcutsDataComponent
 import org.hyperskill.app.notification.remote.data.repository.IosFCMTokenProvider
 
 interface IosAppComponent : AppGraph {
     fun getIosFCMTokenProvider(): IosFCMTokenProvider
+
+    fun buildApplicationShortcutsDataComponent(): ApplicationShortcutsDataComponent
 }
