ad108d271b9708148be82df7da2271526ba8e5b0
Ivan Magda <ivan.magda@hyperskill.org>
2024-02-12 13:01:10 +0800

    Polish up track selection details footer view

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/TrackSelection/Details/Views/Content/TrackSelectionDetailsContentView.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/TrackSelection/Details/Views/Content/TrackSelectionDetailsContentView.swift
@@ -3,18 +3,6 @@ import SwiftUI
 extension TrackSelectionDetailsContentView {
     struct Appearance {
         let spacing = LayoutInsets.defaultInset
-
-        let callToActionBlurInsets = LayoutInsets(
-            horizontal: LayoutInsets.smallInset,
-            vertical: -LayoutInsets.smallInset
-        )
-
-        func makeCallToActionButtonStyle(isEnabled: Bool) -> RoundedRectangleButtonStyle {
-            var style = RoundedRectangleButtonStyle(style: .violet)
-            style.backgroundDisabledOpacity = 1
-            style.foregroundColor = isEnabled ? Color(ColorPalette.onPrimary) : Color(ColorPalette.onPrimaryAlpha60)
-            return style
-        }
     }
 }
 
@@ -42,13 +30,7 @@ struct TrackSelectionDetailsContentView: View {
     let isCallToActionButtonEnabled: Bool
     let onCallToActionButtonTap: () -> Void
 
-    private let callToActionButtonFeedbackGenerator = FeedbackGenerator(feedbackType: .selection)
-
     var body: some View {
-        let callToActionButtonStyle = appearance.makeCallToActionButtonStyle(
-            isEnabled: isCallToActionButtonEnabled
-        )
-
         ScrollView {
             VStack(spacing: appearance.spacing) {
                 TrackSelectionDetailsDescriptionView(
@@ -74,36 +56,33 @@ struct TrackSelectionDetailsContentView: View {
                 )
             }
             .padding()
-            .padding(.bottom, callToActionButtonStyle.minHeight)
         }
         .frame(maxWidth: .infinity)
         .navigationTitle(navigationTitle)
-        .overlay(
-            buildCallToActionButton(buttonStyle: callToActionButtonStyle),
-            alignment: .bottom
-        )
+        .safeAreaInsetBottomCompatibility(footerView)
     }
 
     @MainActor
-    @ViewBuilder
-    private func buildCallToActionButton(
-        buttonStyle: RoundedRectangleButtonStyle
-    ) -> some View {
-        Button(
-            Strings.TrackSelectionDetails.callToActionButtonTitle,
-            action: {
-                callToActionButtonFeedbackGenerator.triggerFeedback()
-                onCallToActionButtonTap()
-            }
-        )
-        .buttonStyle(buttonStyle)
-        .padding(.horizontal)
-        .background(
-            Color(ColorPalette.surface)
-                .padding(appearance.callToActionBlurInsets.edgeInsets)
-                .blur(radius: buttonStyle.cornerRadius)
-        )
-        .disabled(!isCallToActionButtonEnabled)
+    @ViewBuilder private var footerView: some View {
+        if isCallToActionButtonEnabled {
+            Button(
+                Strings.TrackSelectionDetails.callToActionButtonTitle,
+                action: {
+                    FeedbackGenerator(feedbackType: .selection).triggerFeedback()
+                    onCallToActionButtonTap()
+                }
+            )
+            .buttonStyle(.primary)
+            .shineEffect()
+            .padding()
+            .background(
+                TransparentBlurView()
+                    .edgesIgnoringSafeArea(.all)
+            )
+            .fixedSize(horizontal: false, vertical: true)
+        } else {
+            EmptyView()
+        }
     }
 }
 
