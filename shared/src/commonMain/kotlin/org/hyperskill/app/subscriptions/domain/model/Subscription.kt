9494f644cf90df2395a79fe05c75996518e2655e
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2024-02-13 13:18:04 +0100

    Add subscription serialization test

--- shared/src/commonMain/kotlin/org/hyperskill/app/subscriptions/domain/model/Subscription.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/subscriptions/domain/model/Subscription.kt
@@ -3,8 +3,18 @@ package org.hyperskill.app.subscriptions.domain.model
 import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.hyperskill.app.subscriptions.cache.CurrentSubscriptionStateHolderImpl
 
 /**
+ * Represents a user profile.
+ *
+ * Warning!
+ * This model is stored in the cache.
+ * Adding new field or modifying old ones,
+ * check that all fields will be deserialized from cache without an error.
+ * All the new optional fields must have default values.
+ * @see [CurrentSubscriptionStateHolderImpl]
+ *
  * If the [stepsLimitResetTime] is null, then the user doesn't have submissions yet
  */
 @Serializable
