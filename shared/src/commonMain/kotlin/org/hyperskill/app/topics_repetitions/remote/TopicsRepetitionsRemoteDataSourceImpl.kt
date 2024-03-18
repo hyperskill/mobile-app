ec42b061d178b30e387001a0d09e4077ea293fa6
Ivan Magda <ivan.magda@hyperskill.org>
2024-03-18 15:41:23 +0700

    Shared: Support is_in_current_track in topics repetitions (#946)
    ^ALTAPPS-1182

--- shared/src/commonMain/kotlin/org/hyperskill/app/topics_repetitions/remote/TopicsRepetitionsRemoteDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/topics_repetitions/remote/TopicsRepetitionsRemoteDataSourceImpl.kt
@@ -3,6 +3,7 @@ package org.hyperskill.app.topics_repetitions.remote
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import org.hyperskill.app.network.remote.parameterPage
@@ -13,22 +14,33 @@ import org.hyperskill.app.topics_repetitions.domain.model.TopicRepetitionStatist
 import org.hyperskill.app.topics_repetitions.remote.model.TopicsRepetitionStatisticsResponse
 import org.hyperskill.app.topics_repetitions.remote.model.TopicsRepetitionsResponse
 
-class TopicsRepetitionsRemoteDataSourceImpl(
+internal class TopicsRepetitionsRemoteDataSourceImpl(
     private val httpClient: HttpClient
 ) : TopicsRepetitionsRemoteDataSource {
-    override suspend fun getTopicsRepetitions(pageSize: Int, page: Int): Result<List<TopicRepetition>> =
+
+    companion object {
+        private const val PARAM_IS_IN_CURRENT_TRACK = "is_in_current_track"
+    }
+
+    override suspend fun getTopicsRepetitions(
+        pageSize: Int,
+        page: Int,
+        isInCurrentTrack: Boolean
+    ): Result<List<TopicRepetition>> =
         kotlin.runCatching {
             httpClient.get("/api/topics-repetition") {
                 contentType(ContentType.Application.Json)
                 parameterPageSize(pageSize)
                 parameterPage(page)
+                parameter(PARAM_IS_IN_CURRENT_TRACK, isInCurrentTrack)
             }.body<TopicsRepetitionsResponse>().topicsRepetitions
         }
 
-    override suspend fun getTopicsRepetitionStatistics(): Result<TopicRepetitionStatistics> =
+    override suspend fun getTopicsRepetitionStatistics(isInCurrentTrack: Boolean): Result<TopicRepetitionStatistics> =
         kotlin.runCatching {
             httpClient.get("/api/topics-repetition/statistics") {
                 contentType(ContentType.Application.Json)
+                parameter(PARAM_IS_IN_CURRENT_TRACK, isInCurrentTrack)
             }.body<TopicsRepetitionStatisticsResponse>().topicsRepetitionStatistics.first()
         }
 }
