39daff8dec522a122b809136e0f1ea1e96fad429
Ivan Magda <ivan.magda@hyperskill.org>
2024-04-12 17:47:27 +0700

    Sorting quiz

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizSorting/Views/StepQuizSortingView.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizSorting/Views/StepQuizSortingView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct StepQuizSortingView: View {
     @ObservedObject var viewModel: StepQuizSortingViewModel
 
+    @Environment(\.isEnabled) private var isEnabled
+
     var body: some View {
         VStack(alignment: .leading, spacing: LayoutInsets.defaultInset) {
             ForEach(Array(viewModel.viewData.items.enumerated()), id: \.element) { index, item in
@@ -23,17 +25,27 @@ struct StepQuizSortingView: View {
                 )
             }
         }
+        .opacity(isEnabled ? 1 : 0.5)
+        .animation(.easeInOut(duration: 0.33), value: isEnabled)
     }
 }
 
 #if DEBUG
-struct StepQuizSortingView_Previews: PreviewProvider {
-    static var previews: some View {
-        StepQuizSortingAssembly
-            .makePlaceholder()
-            .makeModule()
-            .previewLayout(.sizeThatFits)
-            .padding()
+#Preview {
+    ScrollView {
+        VStack(spacing: LayoutInsets.defaultInset) {
+            StepQuizSortingAssembly
+                .makePlaceholder()
+                .makeModule()
+
+            Divider()
+
+            StepQuizSortingAssembly
+                .makePlaceholder()
+                .makeModule()
+                .disabled(true)
+        }
+        .padding()
     }
 }
 #endif
