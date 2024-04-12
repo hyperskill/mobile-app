7edba8a8f085a27f4179d60b47d6ce35b7aabcd1
Ivan Magda <ivan.magda@hyperskill.org>
2024-04-12 17:55:38 +0700

    Table quiz

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizTable/Views/StepQuizTableView.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizTable/Views/StepQuizTableView.swift
@@ -5,6 +5,8 @@ struct StepQuizTableView: View {
 
     private(set) var panModalPresenter: PanModalPresenter
 
+    @Environment(\.isEnabled) private var isEnabled
+
     var body: some View {
         VStack(spacing: LayoutInsets.defaultInset) {
             ForEach(viewModel.viewData.rows) { row in
@@ -18,6 +20,8 @@ struct StepQuizTableView: View {
             }
         }
         .padding(.bottom, LayoutInsets.smallInset)
+        .opacity(isEnabled ? 1 : 0.5)
+        .animation(.easeInOut(duration: 0.33), value: isEnabled)
     }
 
     private func doSelectColumnsPresentation(for row: StepQuizTableViewData.Row) {
@@ -27,12 +31,21 @@ struct StepQuizTableView: View {
 }
 
 #if DEBUG
-struct StepQuizTableView_Previews: PreviewProvider {
-    static var previews: some View {
-        StepQuizTableAssembly
-            .makePlaceholder(isMultipleChoice: false)
-            .makeModule()
-            .previewLayout(.sizeThatFits)
+#Preview {
+    ScrollView {
+        VStack(spacing: LayoutInsets.defaultInset) {
+            StepQuizTableAssembly
+                .makePlaceholder(isMultipleChoice: false)
+                .makeModule()
+
+            Divider()
+
+            StepQuizTableAssembly
+                .makePlaceholder(isMultipleChoice: false)
+                .makeModule()
+                .disabled(true)
+        }
+        .padding()
     }
 }
 #endif
