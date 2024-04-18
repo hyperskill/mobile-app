1ac655f68b148e928ff5b6bb6ff8ecc32e20765f
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2024-04-18 11:40:25 +0200

    Fix iOS build

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuiz/Views/StepQuizView.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuiz/Views/StepQuizView.swift
@@ -339,7 +339,13 @@ struct StepQuizView: View {
             panModalPresenter.dismissPanModal(
                 condition: { ($0 as? ProblemsLimitReachedModalViewController) != nil }
             )
+        case .stepQuizToolbarViewAction(let stepQuizToolbarViewAction):
+            handleStepQuizToolbarViewAction(viewAction: stepQuizToolbarViewAction.viewAction)
         }
+    }
+    
+    private func handleStepQuizToolbarViewAction(viewAction: StepQuizToolbarFeatureActionViewAction) {
+        // no op
     }
 }
 
