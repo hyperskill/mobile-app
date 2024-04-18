da31d8a58b55ce0ebd39208414938205e1f363ac
Ivan Magda <ivan.magda@hyperskill.org>
2024-04-18 12:07:01 +0700

    Add StepToolbarProgressView

--- iosHyperskillApp/iosHyperskillApp/Sources/Helpers/StyledNavigationController.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Helpers/StyledNavigationController.swift
@@ -2,33 +2,33 @@ import SnapKit
 import UIKit
 
 final class StyledNavigationController: UINavigationController {
-    enum Appearance {
-        static let progressBarHeight: CGFloat = 2
+    private lazy var progressView = StepToolbarProgressView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupProgressView()
     }
+}
 
-    private lazy var progressView: UIProgressView = {
-        let progressView = UIProgressView(progressViewStyle: .bar)
-        progressView.trackTintColor = ColorPalette.onSurfaceAlpha9
-        progressView.progressTintColor = ColorPalette.primary
-        return progressView
-    }()
+// MARK: - UIViewController (StepToolbarProgressView) -
 
+extension StyledNavigationController {
     func setProgress(_ progress: Float, animated: Bool) {
-        setupProgressViewIfNeeded()
-        progressView.setProgress(progress, animated: animated)
+        Thread.dispatchOnMainThread {
+            self.progressView.setHidden(false, animated: animated) {
+                self.progressView.setProgress(progress, animated: animated)
+            }
+        }
     }
 
-    private func setupProgressViewIfNeeded() {
-        guard progressView.superview == nil else {
-            return
-        }
+    private func setupProgressView() {
+        progressView.setHidden(true, animated: false, completion: nil)
 
         navigationBar.addSubview(progressView)
         progressView.translatesAutoresizingMaskIntoConstraints = false
         progressView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview()
             make.bottom.equalTo(navigationBar.snp.bottom)
-            make.height.equalTo(Appearance.progressBarHeight)
         }
     }
 }
---
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/Step/Views/StepToolbarProgressView.swift
@@ -0,0 +1,121 @@
+import SnapKit
+import UIKit
+
+extension StepToolbarProgressView {
+    struct Appearance {
+        let trackTintColor = ColorPalette.onSurfaceAlpha9
+        let progressTintColor = ColorPalette.primary
+
+        let height: CGFloat = 2
+    }
+
+    enum Animation {
+        static let visibilityDuration: TimeInterval = 0.3
+    }
+}
+
+final class StepToolbarProgressView: UIView {
+    let appearance: Appearance
+
+    private lazy var progressView: UIProgressView = {
+        let progressView = UIProgressView(progressViewStyle: .bar)
+        progressView.trackTintColor = appearance.trackTintColor
+        progressView.progressTintColor = appearance.progressTintColor
+        return progressView
+    }()
+
+    init(
+        frame: CGRect = .zero,
+        appearance: Appearance = Appearance()
+    ) {
+        self.appearance = appearance
+        super.init(frame: frame)
+
+        self.setupView()
+        self.addSubviews()
+        self.makeConstraints()
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setProgress(_ progress: Float, animated: Bool) {
+        progressView.setProgress(progress, animated: animated)
+    }
+
+    func setHidden(_ isHidden: Bool, animated: Bool, completion: (() -> Void)?) {
+        // Check if the current hidden state already matches the desired state
+        if self.isHidden == isHidden {
+            completion?()
+            return
+        }
+
+        if isHidden {
+            animateVisibility(shouldHide: true, animated: animated, completion: completion)
+        } else {
+            animateVisibility(shouldHide: false, animated: animated, completion: completion)
+        }
+    }
+
+    private func animateVisibility(shouldHide: Bool, animated: Bool, completion: (() -> Void)?) {
+        isHidden = false
+
+        let initialAlpha: CGFloat = shouldHide ? 1 : 0
+        let finalAlpha: CGFloat = shouldHide ? 0 : 1
+
+        if animated {
+            alpha = initialAlpha
+
+            UIView.animate(
+                withDuration: Animation.visibilityDuration,
+                animations: {
+                    self.alpha = finalAlpha
+                },
+                completion: { _ in
+                    if shouldHide {
+                        self.isHidden = true
+                    }
+
+                    completion?()
+                }
+            )
+        } else {
+            alpha = finalAlpha
+
+            if shouldHide {
+                isHidden = true
+            }
+
+            completion?()
+        }
+    }
+}
+
+extension StepToolbarProgressView: ProgrammaticallyInitializableViewProtocol {
+    func setupView() {}
+
+    func addSubviews() {
+        addSubview(progressView)
+    }
+
+    func makeConstraints() {
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.height.equalTo(appearance.height)
+        }
+    }
+}
+
+// MARK: - Preview -
+
+#if DEBUG
+@available(iOS 17, *)
+#Preview {
+    let view = StepToolbarProgressView()
+    view.setHidden(false, animated: true, completion: nil)
+    return view
+}
+#endif
