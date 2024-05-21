b7dafc4c2e3c8704de231c27014d77e0bfaeef6d
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2024-05-21 17:42:03 +0200

    Remove redundant dependencies

--- androidHyperskillApp/build.gradle.kts
+++ androidHyperskillApp/build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     implementation(libs.android.ui.fragment)
     implementation(libs.android.ui.fragment.ktx)
     implementation(libs.android.lifecycle.runtime)
-    implementation(libs.android.lifecycle.proccess)
     implementation(libs.android.browser)
 
     implementation(libs.kotlin.coroutines.core)
