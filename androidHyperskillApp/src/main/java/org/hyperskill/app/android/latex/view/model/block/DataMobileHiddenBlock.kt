cf234f5fbe597c6ccc29587e6743563b54b0bc29
Aleksandr Zhukov <alexandr.zhukov@hyperskill.org>
2024-05-28 08:07:03 +0400

    Android: Fix <sup> tag rendering in ChoiceStepQuiz (#1062)
    ^ALTAPPS-1231

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/latex/view/model/block/DataMobileHiddenBlock.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/latex/view/model/block/DataMobileHiddenBlock.kt
@@ -1,7 +1,13 @@
 package org.hyperskill.app.android.latex.view.model.block
 
 object DataMobileHiddenBlock : ContentBlock {
+
+    private const val DATA_MOBILE_HIDDEN_TAG = "data-mobile-hidden"
+
     override val header: String = """
         <script type="text/javascript" src="file:///android_asset/scripts/remove_data_mobile_hidden_elements.js"></script>
     """.trimIndent()
+
+    override fun isEnabled(content: String): Boolean =
+        DATA_MOBILE_HIDDEN_TAG in content
 }
