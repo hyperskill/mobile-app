159480f705c0b3e90743bea83a8bfb0aff0b0e61
Aleksandr Zhukov <alexandr.zhukov@hyperskill.org>
2024-06-05 09:12:17 +0400

    Shared: Add purchase error logging & analytic (#1071)
    ^ALTAPPS-1275

--- shared/src/commonMain/kotlin/org/hyperskill/app/purchases/injection/PurchaseComponentImpl.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/purchases/injection/PurchaseComponentImpl.kt
@@ -1,12 +1,14 @@
 package org.hyperskill.app.purchases.injection
 
+import org.hyperskill.app.analytic.domain.interactor.AnalyticInteractor
 import org.hyperskill.app.purchases.domain.interactor.PurchaseInteractor
 import org.hyperskill.app.purchases.domain.model.PurchaseManager
 
 class PurchaseComponentImpl(
-    purchaseManager: PurchaseManager
+    purchaseManager: PurchaseManager,
+    analyticInteractor: AnalyticInteractor
 ) : PurchaseComponent {
 
     override val purchaseInteractor: PurchaseInteractor =
-        PurchaseInteractor(purchaseManager)
+        PurchaseInteractor(purchaseManager, analyticInteractor)
 }
