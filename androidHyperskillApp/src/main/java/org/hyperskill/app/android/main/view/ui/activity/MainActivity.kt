25ea544efd6581692ac5650ca2046a1ec8436f55
Ivan Magda <ivan.magda@hyperskill.org>
2024-06-21 17:07:17 +0900

    Fix ktlint

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/main/view/ui/activity/MainActivity.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/main/view/ui/activity/MainActivity.kt
@@ -31,7 +31,6 @@ import org.hyperskill.app.android.clarity.ClarityDelegate
 import org.hyperskill.app.android.core.extensions.screenOrientation
 import org.hyperskill.app.android.core.view.ui.fragment.ReduxViewLifecycleObserver
 import org.hyperskill.app.android.core.view.ui.navigation.AppNavigationContainer
-import org.hyperskill.app.android.core.view.ui.navigation.requireRouter
 import org.hyperskill.app.android.databinding.ActivityMainBinding
 import org.hyperskill.app.android.first_problem_onboarding.navigation.FirstProblemOnboardingScreen
 import org.hyperskill.app.android.main.view.ui.navigation.MainScreen
