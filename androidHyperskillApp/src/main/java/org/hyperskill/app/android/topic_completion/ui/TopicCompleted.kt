8e7c03b5a2205d3b2a72eb8a8021457845930ff1
Aleksandr Zhukov <alexandr.zhukov@stepik.org>
2024-06-27 12:36:44 +0200

    Apply insets to TopicCompleted screen

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/topic_completion/ui/TopicCompleted.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/topic_completion/ui/TopicCompleted.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -48,7 +49,11 @@ fun TopicCompleted(
         enterTransitionState.targetState = true
     }
     val shimmerState = remember { ShimmerShotState() }
-    Column(modifier.padding(vertical = TopicCompletedDefaults.CONTENT_VERTICAL_PADDING)) {
+    Column(
+        modifier
+            .padding(vertical = TopicCompletedDefaults.CONTENT_VERTICAL_PADDING)
+            .safeDrawingPadding()
+    ) {
         TopicCompletedCloseButton(
             onClick = onCloseClick,
             modifier = Modifier
