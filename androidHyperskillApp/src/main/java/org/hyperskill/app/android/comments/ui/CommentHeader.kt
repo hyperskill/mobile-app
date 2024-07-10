75380b8b276b3ec5db0c1366c85263fd0b8637ef
Aleksandr Zhukov <alexandr.zhukov@hyperskill.org>
2024-07-10 17:25:43 +0200

    Load avatar image

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/comments/ui/CommentHeader.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/comments/ui/CommentHeader.kt
@@ -14,12 +14,19 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
 import org.hyperskill.app.R
+import org.hyperskill.app.android.core.view.ui.widget.compose.infiniteShimmer
 
 @Composable
 fun CommentHeader(
@@ -34,12 +41,7 @@ fun CommentHeader(
             .height(IntrinsicSize.Min),
         horizontalArrangement = Arrangement.spacedBy(CommentDefaults.CommentImagePadding)
     ) {
-        Box(
-            modifier = Modifier
-                .requiredSize(CommentDefaults.CommentImageSize)
-                .clip(CircleShape)
-                .background(colorResource(id = R.color.color_on_surface_alpha_12))
-        )
+        CommentAuthorAvatar(avatarUrl = authorAvatar)
         Column(
             modifier = Modifier.fillMaxHeight(),
             verticalArrangement = Arrangement.SpaceBetween
@@ -63,4 +65,44 @@ fun CommentHeader(
             }
         }
     }
+}
+
+@Composable
+private fun CommentAuthorAvatar(
+    avatarUrl: String,
+    modifier: Modifier = Modifier
+) {
+    SubcomposeAsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(avatarUrl)
+            .crossfade(true)
+            .build(),
+        modifier = modifier
+            .requiredSize(CommentDefaults.CommentImageSize)
+            .clip(CircleShape),
+        loading = {
+            CommentAuthorAvatarPlaceholder(playShimmer = true)
+        },
+        error = {
+            CommentAuthorAvatarPlaceholder(playShimmer = false)
+        },
+        contentDescription = null,
+        contentScale = ContentScale.FillWidth,
+        alignment = Alignment.Center,
+        filterQuality = FilterQuality.High
+    )
+}
+
+@Composable
+private fun CommentAuthorAvatarPlaceholder(
+    playShimmer: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .requiredSize(CommentDefaults.CommentImageSize)
+            .clip(CircleShape)
+            .background(colorResource(id = R.color.color_on_surface_alpha_12))
+            .infiniteShimmer(play = playShimmer)
+    )
 }
