9e137b8775ff265ea304d09c0baf8cd7cdba20e8
Aleksandr Zhukov <alexandr.zhukov@hyperskill.org>
2024-07-10 17:29:01 +0200

    Make comment copyable

--- androidHyperskillApp/src/main/java/org/hyperskill/app/android/comments/ui/Comment.kt
+++ androidHyperskillApp/src/main/java/org/hyperskill/app/android/comments/ui/Comment.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -48,13 +49,15 @@ fun Comment(
             authorFullName = comment.authorFullName,
             formattedTime = comment.formattedTime
         )
-        Text(
-            text = comment.text,
-            style = MaterialTheme.typography.body1,
-            color = colorResource(id = R.color.text_primary),
-            lineHeight = 16.sp,
-            modifier = Modifier.padding(start = CommentDefaults.CommentContentStartPadding)
-        )
+        SelectionContainer {
+            Text(
+                text = comment.text,
+                style = MaterialTheme.typography.body1,
+                color = colorResource(id = R.color.text_primary),
+                lineHeight = 16.sp,
+                modifier = Modifier.padding(start = CommentDefaults.CommentContentStartPadding)
+            )
+        }
         CommentReactions(
             reactions = comment.reactions,
             onReactionClick = currentOnReactionClick,
