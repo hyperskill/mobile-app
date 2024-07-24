c2e7bdabb2dbfd26c41560b4dc059af26ad68007
Ivan Magda <ivan.magda@hyperskill.org>
2024-07-24 15:14:10 +0700

    iOS: Badges wrapping view (#1116)
    ^ALTAPPS-769

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizCodeBlanks/Views/StepQuizCodeBlanksSuggestionsView.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizCodeBlanks/Views/StepQuizCodeBlanksSuggestionsView.swift
@@ -7,30 +7,25 @@ struct StepQuizCodeBlanksSuggestionsView: View {
     let onSuggestionTap: (Suggestion) -> Void
 
     var body: some View {
-        let views = ForEach(suggestions, id: \.self) { suggestion in
-            Button(
-                action: {
-                    onSuggestionTap(suggestion)
-                },
-                label: {
-                    StepQuizCodeBlanksOptionView(text: suggestion.text, isActive: true)
-                }
+        FlowLayoutCompatibility(
+            configuration: .init(
+                spacing: LayoutInsets.defaultInset,
+                fallbackLayout: .vertical()
             )
-            .buttonStyle(BounceButtonStyle())
-        }
-
-        if #available(iOS 16.0, *) {
-            FlowLayout(spacing: LayoutInsets.defaultInset) {
-                views
+        ) {
+            ForEach(suggestions, id: \.self) { suggestion in
+                Button(
+                    action: {
+                        onSuggestionTap(suggestion)
+                    },
+                    label: {
+                        StepQuizCodeBlanksOptionView(text: suggestion.text, isActive: true)
+                    }
+                )
+                .buttonStyle(BounceButtonStyle())
             }
-            .padding(LayoutInsets.defaultInset)
-            .frame(minHeight: 72)
-        } else {
-            VStack(alignment: .leading, spacing: LayoutInsets.defaultInset) {
-                views
-            }
-            .padding(LayoutInsets.defaultInset)
-            .frame(minHeight: 72)
         }
+        .padding(LayoutInsets.defaultInset)
+        .frame(minHeight: 72)
     }
 }
