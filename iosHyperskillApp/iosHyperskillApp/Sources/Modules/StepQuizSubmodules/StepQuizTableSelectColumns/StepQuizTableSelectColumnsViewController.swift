2316ffee542774f8837a0ca474c55e88389e876e
Ivan Magda <ivan.magda@hyperskill.org>
2024-07-26 08:55:58 +0700

    iOS: Fix failed to show full content length in table problem bottom sheet (#1121)
    ^ALTAPPS-1294

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizTableSelectColumns/StepQuizTableSelectColumnsViewController.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizTableSelectColumns/StepQuizTableSelectColumnsViewController.swift
@@ -1,5 +1,4 @@
 import PanModal
-import SwiftUI
 import UIKit
 
 extension StepQuizTableSelectColumnsViewController {
@@ -8,11 +7,18 @@ extension StepQuizTableSelectColumnsViewController {
     }
 }
 
-final class StepQuizTableSelectColumnsViewController: PanModalSwiftUIViewController<StepQuizTableSelectColumnsView> {
+final class StepQuizTableSelectColumnsViewController: PanModalPresentableViewController {
+    private let rowTitle: String
     private let columns: [StepQuizTableViewData.Column]
     private var selectedColumnsIDs: Set<Int>
     private let isMultipleChoice: Bool
     private let onColumnsSelected: (Set<Int>) -> Void
+
+    var tableQuizSelectColumnsView: StepQuizTableSelectColumnsView? { self.view as? StepQuizTableSelectColumnsView }
+
+    override var panScrollable: UIScrollView? { tableQuizSelectColumnsView?.panScrollable }
+
+    override var shortFormHeight: PanModalHeight { longFormHeight }
 
     init(
         title: String,
@@ -21,43 +27,76 @@ final class StepQuizTableSelectColumnsViewController: PanModalSwiftUIViewControl
         isMultipleChoice: Bool,
         onColumnsSelected: @escaping (Set<Int>) -> Void
     ) {
+        self.rowTitle = title
         self.columns = columns
         self.selectedColumnsIDs = selectedColumnsIDs
         self.isMultipleChoice = isMultipleChoice
         self.onColumnsSelected = onColumnsSelected
 
+        super.init()
+    }
+
+    override func loadView() {
+        let view = StepQuizTableSelectColumnsView(frame: UIScreen.main.bounds)
+        self.view = view
+        view.delegate = self
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
         let prompt = isMultipleChoice
             ? Strings.StepQuizTable.multipleChoicePrompt
             : Strings.StepQuizTable.singleChoicePrompt
+        tableQuizSelectColumnsView?.prompt = prompt
+        tableQuizSelectColumnsView?.isMultipleChoice = isMultipleChoice
 
-        var view = StepQuizTableSelectColumnsView(
-            prompt: prompt,
-            title: title,
+        tableQuizSelectColumnsView?.set(
+            title: rowTitle,
             columns: columns,
-            selectedColumnsIDs: selectedColumnsIDs,
-            isMultipleChoice: isMultipleChoice
+            selectedColumnsIDs: selectedColumnsIDs
         )
 
-        super.init(
-            isPresented: .constant(false),
-            content: { view }
-        )
+        panModalSetNeedsLayoutUpdate()
+    }
+}
 
-        view.onColumnsChanged = self.handleColumnsChanged(_:)
-        view.onConfirmTapped = self.finishColumnsSelection
+extension StepQuizTableSelectColumnsViewController: StepQuizTableSelectColumnsViewDelegate {
+    func tableQuizSelectColumnsView(
+        _ view: StepQuizTableSelectColumnsView,
+        didSelectColumn column: StepQuizTableViewData.Column,
+        isOn: Bool
+    ) {
+        if isMultipleChoice {
+            if isOn {
+                selectedColumnsIDs.insert(column.id)
+            } else {
+                selectedColumnsIDs.remove(column.id)
+            }
+        } else {
+            assert(selectedColumnsIDs.count <= 1, "Sigle choice")
+            selectedColumnsIDs.removeAll()
+
+            if isOn {
+                selectedColumnsIDs.insert(column.id)
+            }
+        }
+
+        tableQuizSelectColumnsView?.update(selectedColumnsIDs: selectedColumnsIDs)
     }
 
-    // MARK: Private API
-
-    private func handleColumnsChanged(_ newColumns: Set<Int>) {
-        self.selectedColumnsIDs = newColumns
-    }
-
-    private func finishColumnsSelection() {
-        self.onColumnsSelected(self.selectedColumnsIDs)
+    func tableQuizSelectColumnsViewDidTapConfirm(_ view: StepQuizTableSelectColumnsView) {
+        onColumnsSelected(selectedColumnsIDs)
 
         DispatchQueue.main.asyncAfter(deadline: .now() + Animation.dismissAnimationDelay) {
             self.dismiss(animated: true)
+        }
+    }
+
+    func tableQuizSelectColumnsViewDidLoadContent(_ view: StepQuizTableSelectColumnsView) {
+        DispatchQueue.main.async {
+            self.panModalSetNeedsLayoutUpdate()
+            self.panModalTransition(to: .longForm)
         }
     }
 }
