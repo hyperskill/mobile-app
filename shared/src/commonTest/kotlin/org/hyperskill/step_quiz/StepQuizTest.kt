e8cd6de0523057454c6f8775bd251296bbc3076c
Aleksandr Zhukov <alexandr.zhukov@hyperskill.org>
2024-07-29 12:23:07 +0200

    Update tests

--- shared/src/commonTest/kotlin/org/hyperskill/step_quiz/StepQuizTest.kt
+++ shared/src/commonTest/kotlin/org/hyperskill/step_quiz/StepQuizTest.kt
@@ -774,7 +774,7 @@ class StepQuizTest {
     }
 
     @Test
-    fun `Wrong and rejected submissions should incremented on CreateSubmissionSuccess`() {
+    fun `Wrong and rejected submissions should increment wrongSubmissionsCount on CreateSubmissionSuccess`() {
         val step = Step.stub(id = 1)
         val attempt = Attempt.stub()
         val submissionState = StepQuizFeature.SubmissionState.Loaded(
@@ -820,7 +820,7 @@ class StepQuizTest {
     }
 
     @Test
-    fun `Wrong and rejected submissions should be kept on CreateAttemptSuccess`() {
+    fun `WrongSubmissionsCount submissions should be kept on CreateAttemptSuccess`() {
         val step = Step.stub(id = 1)
         val attempt = Attempt.stub()
         val submissionState = StepQuizFeature.SubmissionState.Loaded(
@@ -863,5 +863,52 @@ class StepQuizTest {
             expectedWrongSubmissionsCount,
             (state.stepQuizState as? StepQuizFeature.StepQuizState.AttemptLoaded)?.wrongSubmissionsCount
         )
+    }
+
+    @Test
+    fun `Non-error submissions should not increment wrongSubmissionsCount`() {
+        val step = Step.stub(id = 1)
+        val attempt = Attempt.stub()
+        val submissionState = StepQuizFeature.SubmissionState.Loaded(
+            Submission.stub(status = SubmissionStatus.EVALUATION)
+        )
+        val stepRoute = StepRoute.Learn.Step(step.id, null)
+
+        val expectedWrongSubmissionsCount = 0
+
+        val reducer = StepQuizReducer(
+            stepRoute = stepRoute,
+            stepQuizChildFeatureReducer = StepQuizChildFeatureReducer.stub(stepRoute)
+        )
+
+        listOf(
+            SubmissionStatus.LOCAL,
+            SubmissionStatus.EVALUATION,
+            SubmissionStatus.CORRECT
+        ).forEach { submissionStatus ->
+            val (state, _) = reducer.reduce(
+                StepQuizFeature.State(
+                    stepQuizState = StepQuizFeature.StepQuizState.AttemptLoaded(
+                        step = step,
+                        attempt = attempt,
+                        submissionState = submissionState,
+                        isProblemsLimitReached = false,
+                        isTheoryAvailable = false,
+                        wrongSubmissionsCount = 0
+                    ),
+                    stepQuizHintsState = StepQuizHintsFeature.State.Idle,
+                    stepQuizToolbarState = StepQuizToolbarFeature.initialState(stepRoute),
+                    stepQuizCodeBlanksState = StepQuizCodeBlanksFeature.initialState()
+                ),
+                StepQuizFeature.Message.CreateSubmissionSuccess(
+                    submission = Submission.stub(status = submissionStatus)
+                )
+            )
+
+            assertEquals(
+                expectedWrongSubmissionsCount,
+                (state.stepQuizState as? StepQuizFeature.StepQuizState.AttemptLoaded)?.wrongSubmissionsCount
+            )
+        }
     }
 }
