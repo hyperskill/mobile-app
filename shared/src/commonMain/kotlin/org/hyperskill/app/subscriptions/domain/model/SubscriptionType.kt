f78d1f5d0bb92f6c8c594ddcc3f3a0da1b23cd6f
Aleksandr Zhukov <alexandr.zhukov@hyperskill.org>
2024-08-02 12:18:29 +0200

    Fix subscription management is not available for MobileContentTrial subscription

--- shared/src/commonMain/kotlin/org/hyperskill/app/subscriptions/domain/model/SubscriptionType.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/subscriptions/domain/model/SubscriptionType.kt
@@ -9,6 +9,8 @@ enum class SubscriptionType(
     val isProjectInfoAvailable: Boolean = true,
     val isCertificateAvailable: Boolean = true,
     val areHintsLimited: Boolean = false,
+    val isSubscriptionSettingsVisible: Boolean = false,
+    val canUpgradeToMobileOnly: Boolean = false,
     val subscriptionLimitType: SubscriptionLimitType = SubscriptionLimitType.NONE
 ) {
     @SerialName("personal")
@@ -39,12 +41,16 @@ enum class SubscriptionType(
         isCertificateAvailable = false,
         isProjectInfoAvailable = false,
         areHintsLimited = true,
+        isSubscriptionSettingsVisible = true,
+        canUpgradeToMobileOnly = true,
         subscriptionLimitType = SubscriptionLimitType.PROBLEMS
     ),
     MOBILE_CONTENT_TRIAL(
         isCertificateAvailable = false,
         isProjectInfoAvailable = false,
         areHintsLimited = true,
+        isSubscriptionSettingsVisible = true,
+        canUpgradeToMobileOnly = true,
         subscriptionLimitType = SubscriptionLimitType.TOPICS
     ),
 
@@ -52,7 +58,8 @@ enum class SubscriptionType(
     MOBILE_ONLY(
         isCertificateAvailable = false,
         isProjectInfoAvailable = false,
-        areHintsLimited = true
+        areHintsLimited = true,
+        isSubscriptionSettingsVisible = true,
     ),
 
     @SerialName("premium")
