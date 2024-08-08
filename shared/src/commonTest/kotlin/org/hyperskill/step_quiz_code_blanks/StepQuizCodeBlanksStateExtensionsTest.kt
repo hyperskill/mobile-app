3995683edcbdd96ec98ef158355bb16d7d67df47
Ivan Magda <ivan.magda@hyperskill.org>
2024-08-08 17:04:12 +0900

    Fix tests

--- shared/src/commonTest/kotlin/org/hyperskill/step_quiz_code_blanks/StepQuizCodeBlanksStateExtensionsTest.kt
+++ shared/src/commonTest/kotlin/org/hyperskill/step_quiz_code_blanks/StepQuizCodeBlanksStateExtensionsTest.kt
@@ -2,14 +2,18 @@ package org.hyperskill.step_quiz_code_blanks
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 import org.hyperskill.app.step.domain.model.Block
 import org.hyperskill.app.step.domain.model.Step
 import org.hyperskill.app.step_quiz_code_blanks.domain.model.CodeBlock
+import org.hyperskill.app.step_quiz_code_blanks.domain.model.CodeBlockChild
 import org.hyperskill.app.step_quiz_code_blanks.domain.model.Suggestion
 import org.hyperskill.app.step_quiz_code_blanks.presentation.StepQuizCodeBlanksFeature
 import org.hyperskill.app.step_quiz_code_blanks.presentation.activeCodeBlockIndex
 import org.hyperskill.app.step_quiz_code_blanks.presentation.createReply
+import org.hyperskill.app.step_quiz_code_blanks.presentation.isVariableSuggestionsAvailable
 import org.hyperskill.app.submissions.domain.model.Reply
 import org.hyperskill.step.domain.model.stub
 
@@ -18,11 +22,15 @@ class StepQuizCodeBlanksStateExtensionsTest {
     fun `activeCodeBlockIndex should return null if no active code block`() {
         val state = stubContentState(
             codeBlocks = listOf(
-                CodeBlock.Blank(isActive = false),
+                CodeBlock.Blank(isActive = false, suggestions = emptyList()),
                 CodeBlock.Print(
-                    isActive = false,
-                    suggestions = emptyList(),
-                    selectedSuggestion = null
+                    children = listOf(
+                        CodeBlockChild.SelectSuggestion(
+                            isActive = false,
+                            suggestions = emptyList(),
+                            selectedSuggestion = null
+                        )
+                    )
                 )
             )
         )
@@ -33,11 +41,15 @@ class StepQuizCodeBlanksStateExtensionsTest {
     fun `activeCodeBlockIndex should return index of the active code block`() {
         val state = stubContentState(
             codeBlocks = listOf(
-                CodeBlock.Blank(isActive = false),
+                CodeBlock.Blank(isActive = false, suggestions = emptyList()),
                 CodeBlock.Print(
-                    isActive = true,
-                    suggestions = emptyList(),
-                    selectedSuggestion = null
+                    children = listOf(
+                        CodeBlockChild.SelectSuggestion(
+                            isActive = true,
+                            suggestions = emptyList(),
+                            selectedSuggestion = null
+                        )
+                    )
                 )
             )
         )
@@ -48,11 +60,15 @@ class StepQuizCodeBlanksStateExtensionsTest {
     fun `createReply should return Reply with code from code blocks and language from step options`() {
         val codeBlocks = listOf(
             CodeBlock.Print(
-                isActive = false,
-                suggestions = emptyList(),
-                selectedSuggestion = Suggestion.ConstantString("\"test\"")
+                children = listOf(
+                    CodeBlockChild.SelectSuggestion(
+                        isActive = false,
+                        suggestions = emptyList(),
+                        selectedSuggestion = Suggestion.ConstantString("\"test\"")
+                    )
+                )
             ),
-            CodeBlock.Blank(isActive = true)
+            CodeBlock.Blank(isActive = true, suggestions = emptyList())
         )
         val step = Step.stub(id = 1).copy(
             block = Block.stub(
@@ -69,6 +85,36 @@ class StepQuizCodeBlanksStateExtensionsTest {
         val expectedReply = Reply.code(code = "print(\"test\")\n", language = "python3")
 
         assertEquals(expectedReply, state.createReply())
+    }
+
+    @Test
+    fun `isVariableSuggestionsAvailable should return true if variable suggestions are available`() {
+        val step = Step.stub(
+            id = 1,
+            block = Block.stub(options = Block.Options(codeBlanksVariables = listOf("a", "b")))
+        )
+        val state = stubContentState(
+            step = step,
+            codeBlocks = emptyList()
+        )
+
+        assertTrue(state.isVariableSuggestionsAvailable)
+    }
+
+    @Test
+    fun `isVariableSuggestionsAvailable should return false if variable suggestions are not available`() {
+        listOf(null, emptyList<String>()).forEach { codeBlanksVariables ->
+            val step = Step.stub(
+                id = 1,
+                block = Block.stub(options = Block.Options(codeBlanksVariables = codeBlanksVariables))
+            )
+            val state = stubContentState(
+                step = step,
+                codeBlocks = emptyList()
+            )
+
+            assertFalse(state.isVariableSuggestionsAvailable)
+        }
     }
 
     private fun stubContentState(
