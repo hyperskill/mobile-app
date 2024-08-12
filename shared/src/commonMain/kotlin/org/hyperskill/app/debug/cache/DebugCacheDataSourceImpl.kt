753fb3bc851486e645854373f371f6e9b8c1c8fe
Ivan Magda <ivan.magda@hyperskill.org>
2024-08-12 12:33:40 +0900

    HQA-552: Return EndpointConfigType.MAIN for internal testing by default

--- shared/src/commonMain/kotlin/org/hyperskill/app/debug/cache/DebugCacheDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/debug/cache/DebugCacheDataSourceImpl.kt
@@ -1,15 +1,26 @@
 package org.hyperskill.app.debug.cache
 
 import com.russhwolf.settings.Settings
+import org.hyperskill.app.config.BuildKonfig
 import org.hyperskill.app.debug.data.source.DebugCacheDataSource
 import org.hyperskill.app.debug.domain.model.EndpointConfigType
 
-class DebugCacheDataSourceImpl(private val settings: Settings) : DebugCacheDataSource {
+internal class DebugCacheDataSourceImpl(
+    private val settings: Settings
+) : DebugCacheDataSource {
     override fun getEndpointConfig(): EndpointConfigType {
+        val defaultEndpointConfigType =
+            if (BuildKonfig.IS_INTERNAL_TESTING == true) {
+                EndpointConfigType.MAIN
+            } else {
+                EndpointConfigType.PRODUCTION
+            }
+
         val cachedEndpointConfigName = settings.getString(
             key = DebugCacheKeyValues.ENDPOINT_CONFIG,
-            defaultValue = EndpointConfigType.PRODUCTION.name
+            defaultValue = defaultEndpointConfigType.name
         )
+
         return EndpointConfigType.valueOf(cachedEndpointConfigName)
     }
 
