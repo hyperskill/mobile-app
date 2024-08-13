60ee85d7851374479d9c763b13f41ed5526dbbf1
Ivan Magda <ivan.magda@hyperskill.org>
2024-08-13 13:40:21 +0900

    Update controls font

--- iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizTable/Views/StepQuizTableRowView.swift
+++ iosHyperskillApp/iosHyperskillApp/Sources/Modules/StepQuizSubmodules/StepQuizTable/Views/StepQuizTableRowView.swift
@@ -10,8 +10,6 @@ extension StepQuizTableRowView {
 
         let interItemSpacing = LayoutInsets.smallInset
 
-        let chevronRightIconWidthHeight: CGFloat = 24
-
         let minHeight: CGFloat = 44
     }
 }
@@ -23,41 +21,39 @@ struct StepQuizTableRowView: View {
 
     var subtitle: String?
 
-    var onTap: () -> Void
+    var action: () -> Void
 
     var body: some View {
-        Button(action: onTap) {
-            HStack(spacing: appearance.interItemSpacing) {
-                VStack(alignment: .leading, spacing: appearance.interItemSpacing) {
-                    Group {
+        Button(action: action) {
+            VStack(alignment: .leading, spacing: appearance.interItemSpacing) {
+                Group {
+                    LatexView(
+                        text: title,
+                        configuration: .quizContent(
+                            textFont: appearance.titleFont,
+                            textColor: appearance.titleTextColor
+                        )
+                    )
+
+                    if let subtitle, !subtitle.isEmpty {
                         LatexView(
-                            text: title,
+                            text: subtitle,
                             configuration: .quizContent(
-                                textFont: appearance.titleFont,
-                                textColor: appearance.titleTextColor
+                                textFont: appearance.subtitleFont,
+                                textColor: appearance.subtitleTextColor
                             )
                         )
-
-                        if let subtitle, !subtitle.isEmpty {
-                            LatexView(
-                                text: subtitle,
-                                configuration: .quizContent(
-                                    textFont: appearance.subtitleFont,
-                                    textColor: appearance.subtitleTextColor
-                                )
-                            )
-                        }
                     }
-                    .multilineTextAlignment(.leading)
-                    .frame(maxWidth: .infinity, alignment: .leading)
                 }
+                .multilineTextAlignment(.leading)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
 
-                Button(action: onTap) {
-                    Image(systemName: "chevron.right")
-                        .imageScale(.large)
-                        .frame(widthHeight: appearance.chevronRightIconWidthHeight)
-                        .foregroundColor(Color(ColorPalette.onSurfaceAlpha38))
-                }
+            Button(action: action) {
+                Image(systemName: "chevron.right")
+                    .font(.title2.bold())
+                    .padding(.vertical, LayoutInsets.smallInset / 2)
+                    .foregroundColor(Color(ColorPalette.primary))
             }
         }
         .frame(maxWidth: .infinity, minHeight: appearance.minHeight)
@@ -67,11 +63,16 @@ struct StepQuizTableRowView: View {
 struct StepQuizTableRowView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            StepQuizTableRowView(title: "Variant A", subtitle: "Variant Option", onTap: {})
+            ScrollView {
+                VStack {
+                    StepQuizTableRowView(title: "Variant A", subtitle: "Variant Option", action: {})
+                    Spacer()
+                }
+            }
 
-            StepQuizTableRowView(title: "Variant A", subtitle: nil, onTap: {})
+            StepQuizTableRowView(title: "Variant A", subtitle: nil, action: {})
 
-            StepQuizTableRowView(title: "Variant A", subtitle: "Variant Option", onTap: {})
+            StepQuizTableRowView(title: "Variant A", subtitle: "Variant Option", action: {})
                 .preferredColorScheme(.dark)
         }
         .previewLayout(.sizeThatFits)
