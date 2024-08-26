d92f3ccac0d9d7b85a942c81d00a313b842c1555
Ivan Magda <ivan.magda@hyperskill.org>
2024-08-26 18:20:31 +0800

    shared: Rollback auto choosing Python track (#1164)
    ^ALTAPPS-1338

--- shared/src/commonMain/kotlin/org/hyperskill/app/welcome_onboarding/root/presentation/WelcomeOnboardingReducer.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/welcome_onboarding/root/presentation/WelcomeOnboardingReducer.kt
@@ -83,9 +83,7 @@ internal class WelcomeOnboardingReducer : StateReducer<State, Message, Action> {
                 WelcomeQuestionnaireType.LEARNING_REASON ->
                     NavigateTo.WelcomeOnboardingQuestionnaire(WelcomeQuestionnaireType.CODING_EXPERIENCE)
                 WelcomeQuestionnaireType.CODING_EXPERIENCE ->
-                    // NavigateTo.ChooseProgrammingLanguage
-                    // ALTAPPS-1328: Redirect to Python track directly
-                    NavigateTo.TrackDetails(WelcomeOnboardingTrack.PYTHON)
+                    NavigateTo.ChooseProgrammingLanguage
             }
         )
 
