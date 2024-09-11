3209444b48b5058474b8c8adc9622ea2ead8beb0
Ivan Magda <ivan.magda@hyperskill.org>
2024-09-11 16:55:51 +0900

    Update StepQuizCodeBlanksReducerCodeBlockClickedTest

--- shared/src/commonTest/kotlin/org/hyperskill/step_quiz_code_blanks/presentation/StepQuizCodeBlanksReducerCodeBlockClickedTest.kt
+++ shared/src/commonTest/kotlin/org/hyperskill/step_quiz_code_blanks/presentation/StepQuizCodeBlanksReducerCodeBlockClickedTest.kt
@@ -27,6 +27,36 @@ class StepQuizCodeBlanksReducerCodeBlockClickedTest {
     }
 
     @Test
+    fun `CodeBlockClicked should not update state if no target code block`() {
+        val initialState = StepQuizCodeBlanksFeature.State.Content.stub(
+            codeBlocks = listOf(CodeBlock.Blank(isActive = true, suggestions = emptyList()))
+        )
+
+        val message = StepQuizCodeBlanksFeature.Message.CodeBlockClicked(
+            codeBlockItem = StepQuizCodeBlanksViewState.CodeBlockItem.Blank(id = 1, isActive = true)
+        )
+        val (state, actions) = reducer.reduce(initialState, message)
+
+        assertEquals(initialState, state)
+        assertContainsCodeBlockClickedAnalyticEvent(actions)
+    }
+
+    @Test
+    fun `CodeBlockClicked should not update state if target code block is active`() {
+        val initialState = StepQuizCodeBlanksFeature.State.Content.stub(
+            codeBlocks = listOf(CodeBlock.Blank(isActive = true, suggestions = emptyList()))
+        )
+
+        val message = StepQuizCodeBlanksFeature.Message.CodeBlockClicked(
+            codeBlockItem = StepQuizCodeBlanksViewState.CodeBlockItem.Blank(id = 0, isActive = true)
+        )
+        val (state, actions) = reducer.reduce(initialState, message)
+
+        assertEquals(initialState, state)
+        assertContainsCodeBlockClickedAnalyticEvent(actions)
+    }
+
+    @Test
     fun `CodeBlockClicked should update active Print code block`() {
         val initialState = StepQuizCodeBlanksFeature.State.Content.stub(
             codeBlocks = listOf(
@@ -64,7 +94,32 @@ class StepQuizCodeBlanksReducerCodeBlockClickedTest {
     }
 
     @Test
-    fun `CodeBlockClicked should update active Variable code block`() {
+    fun `CodeBlockClicked should update active ElseStatement code block`() {
+        val initialState = StepQuizCodeBlanksFeature.State.Content.stub(
+            codeBlocks = listOf(
+                CodeBlock.Blank(isActive = false, suggestions = emptyList()),
+                CodeBlock.ElseStatement(isActive = true)
+            )
+        )
+
+        val message = StepQuizCodeBlanksFeature.Message.CodeBlockClicked(
+            codeBlockItem = StepQuizCodeBlanksViewState.CodeBlockItem.Blank(id = 0, isActive = false)
+        )
+        val (state, actions) = reducer.reduce(initialState, message)
+
+        val expectedState = initialState.copy(
+            codeBlocks = listOf(
+                CodeBlock.Blank(isActive = true, suggestions = emptyList()),
+                CodeBlock.ElseStatement(isActive = false)
+            )
+        )
+
+        assertEquals(expectedState, state)
+        assertContainsCodeBlockClickedAnalyticEvent(actions)
+    }
+
+    @Test
+    fun `CodeBlockClicked should update active Variable code block to not active`() {
         val initialState = StepQuizCodeBlanksFeature.State.Content.stub(
             codeBlocks = listOf(
                 CodeBlock.Print(
@@ -105,6 +160,61 @@ class StepQuizCodeBlanksReducerCodeBlockClickedTest {
                     children = listOf(
                         CodeBlockChild.SelectSuggestion(
                             isActive = false, suggestions = emptyList(), selectedSuggestion = null
+                        ),
+                        CodeBlockChild.SelectSuggestion(
+                            isActive = false, suggestions = emptyList(), selectedSuggestion = null
+                        )
+                    )
+                )
+            )
+        )
+
+        assertEquals(expectedState, state)
+        assertContainsCodeBlockClickedAnalyticEvent(actions)
+    }
+
+    @Test
+    fun `CodeBlockClicked should update not active Variable code block to active`() {
+        val initialState = StepQuizCodeBlanksFeature.State.Content.stub(
+            codeBlocks = listOf(
+                CodeBlock.Print(
+                    children = listOf(
+                        CodeBlockChild.SelectSuggestion(
+                            isActive = true, suggestions = emptyList(), selectedSuggestion = null
+                        )
+                    )
+                ),
+                CodeBlock.Variable(
+                    children = listOf(
+                        CodeBlockChild.SelectSuggestion(
+                            isActive = false, suggestions = emptyList(), selectedSuggestion = null
+                        ),
+                        CodeBlockChild.SelectSuggestion(
+                            isActive = false, suggestions = emptyList(), selectedSuggestion = null
+                        )
+                    )
+                )
+            )
+        )
+
+        val message = StepQuizCodeBlanksFeature.Message.CodeBlockClicked(
+            codeBlockItem = StepQuizCodeBlanksViewState.CodeBlockItem.Blank(id = 1, isActive = false)
+        )
+        val (state, actions) = reducer.reduce(initialState, message)
+
+        val expectedState = initialState.copy(
+            codeBlocks = listOf(
+                CodeBlock.Print(
+                    children = listOf(
+                        CodeBlockChild.SelectSuggestion(
+                            isActive = false, suggestions = emptyList(), selectedSuggestion = null
+                        )
+                    )
+                ),
+                CodeBlock.Variable(
+                    children = listOf(
+                        CodeBlockChild.SelectSuggestion(
+                            isActive = true, suggestions = emptyList(), selectedSuggestion = null
                         ),
                         CodeBlockChild.SelectSuggestion(
                             isActive = false, suggestions = emptyList(), selectedSuggestion = null
