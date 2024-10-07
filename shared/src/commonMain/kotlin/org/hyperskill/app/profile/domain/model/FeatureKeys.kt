0c86f7539bad8f467e4d0898aafc5aa23e8d238f
Ivan Magda <ivan.magda@hyperskill.org>
2024-10-07 15:53:42 +0900

    Revert "Shared: Python adopted course selection (#1181)" (#1198)
    ^ALTAPPS-1361 This reverts commit c49c6f6eb6f3e8bb2ad1e32edfbc51ecc4a498e4.

--- shared/src/commonMain/kotlin/org/hyperskill/app/profile/domain/model/FeatureKeys.kt
+++ shared/src/commonMain/kotlin/org/hyperskill/app/profile/domain/model/FeatureKeys.kt
@@ -11,5 +11,4 @@ internal object FeatureKeys {
     const val MOBILE_ONLY_SUBSCRIPTION = "mobile.mobile_only_subscription"
     const val MOBILE_USERS_INTERVIEW_WIDGET = "mobile.users_interview_widget"
     const val MOBILE_CONTENT_TRIAL = "mobile.content_trial"
-    const val MOBILE_PYTHON_ADOPTED_COURSE = "mobile.python_adopted_course"
 }
